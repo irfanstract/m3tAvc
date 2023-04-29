395a1cbc03f4261c2cbaafaf515c9f982b11b9a2
meh <meh@meh>
2023-04-29 09:21:41 +0700

    `src/cbsq/avc/upstream` - init `common.scala`

---
+++ src/main/scala/cbsq/avc/upstream/common.scala
@@ -0,0 +1,67 @@
+package cbsq.avc.upstream
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//export collection.mutable.{Buffer as Sequencer }
+//export collection.mutable.{ArrayBuffer as ArraySequencer }
+//
+//@annotation.experimental
+//def ??? = scala.Predef.???
+//
+//export java.time.Duration
+//
+//object CAS {
+//
+//   export java.util.concurrent.SynchronousQueue
+//   export java.util.concurrent.BlockingQueue
+//   export java.util.concurrent.BlockingDeque
+//   export java.util.concurrent.{LinkedBlockingDeque, ArrayBlockingQueue}
+//   
+//   export java.util.concurrent.Semaphore
+//   export java.util.concurrent.locks.{ReadWriteLock, ReentrantReadWriteLock }
+//
+//   import java.util.concurrent.{atomic as jca}
+//
+//   export jca.{AtomicReference        }
+//   export jca.{AtomicLong             }
+//   export jca.{AtomicInteger          }
+//   export jca.{AtomicBoolean          }
+//   export jca.{AtomicStampedReference }
+//   export jca.{AtomicLongArray        }
+//
+//   export java.lang.invoke.VarHandle.{fullFence}
+//   
+//   export java.lang.ref.Reference.reachabilityFence
+//
+//}
+//
+//export java.net.URI
+//export java.net.{InetAddress, InetSocketAddress }
+//
+//export cbsq.ByteBlob
+//
+
+export cbsq.mCommons.{URL => _, * }
+
+
+
+
+
+
+
+
+
+
+
