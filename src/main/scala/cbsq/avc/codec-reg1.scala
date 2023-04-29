045c34dc2a00c8df6b77745264ae00b610b6161e
meh <meh@meh>
2023-04-29 19:44:02 +0700

    `src/cbsq/avc` - `codec-reg1.scala` - init `CodecTemplateOps.ioUtil` this way

--- src/main/scala/cbsq/avc/codec-reg1.scala
+++ src/main/scala/cbsq/avc/codec-reg1.scala
@@ -65,6 +65,28 @@ object CodecTemplateOps
       class Mismatch        extends java.io.IOException
    }
 
+   object ioUtil {
+      
+      type ReaderFor[B <: java.nio.Buffer | AnyVal] <: (
+         AnyRef
+         & java.io.Closeable
+      ) = 
+         B match {
+            case (java.nio.ByteBuffer   | Byte  ) => java.io.InputStream
+            case (java.nio.CharBuffer   | Char  ) => java.io.Reader
+         }
+
+      type AsBuffered[B <: java.io.Closeable] <: (
+         AnyRef
+         & java.io.Closeable
+      ) = 
+         B match {
+            case (java.io.InputStream) => java.io.BufferedInputStream
+            case (java.io.Reader     ) => java.io.BufferedReader
+         }
+
+   }
+   
 }
 
 
