2bda060a1903037ac5958f4f9cb4c5b24975d106
meh <meh@meh>
2023-05-01 20:41:13 +0700

    `src/cbsq/avc` - `codec-reg1.scala` - `BbsdZcdImpl` - remove `template.instance`, `Instance1AC`, `Instance1`

--- src/main/scala/cbsq/avc/codec-reg1.scala
+++ src/main/scala/cbsq/avc/codec-reg1.scala
@@ -140,10 +140,6 @@ sealed trait  BbsdZcdImpl[
       // thisCodecInstantiable: thisCodecBackend.template.type =>
       thisCodecInstantiable =>
 
-      type Instance 
-         >: thisCodecBackend.Instance1AC 
-         <: thisCodecBackend.Instance1AC
-
       /**
        * 
        */
@@ -219,45 +215,6 @@ sealed trait  BbsdZcdImpl[
       args1: IndexedSeq[String] ,
    ) : Instance1AC
    
-   /* the definitions needed to be split, to avoid compiler crash */
-   // sealed 
-   trait Instance1AC extends 
-      AnyRef
-      with java.io.Closeable 
-      // TODO - 
-      with CodecInstanceOps[template.type]
-      with CodecInstanceOps.FromUrl1
-   { thisCodecInstance =>
-      
-      val associatedCodec: template.type = template
-
-      lazy val decodedFormO: Option[BbsdAvFrameIterator.OfRegOfHandlerForMediaKind[thisCodecBackend.mediaKindP.type ] ]
-      
-   }
-
-   abstract 
-   class Instance1() 
-      extends 
-      AnyRef
-      with java.io.Closeable
-      // blocked by the compiler crash
-      // TODO - 
-      // with CodecInstanceOps[template.type]
-      // with CodecInstanceOps.FromUrl1
-      with Instance1AC
-   {
-
-      override 
-      lazy val events: Iterator[EventInfo] = 
-         Iterator()
-
-      def close() : Unit = {}
-      
-      override def toString(): String = 
-         s"YyCodecRun(.....)@${hashCode }"
-      
-   }
-
 }
 
 
