a5e0d457bb18fcedb5df9d9867facb762b0acb1a
meh <meh@meh>
2023-05-05 17:11:34 +0700

    apply these changes

---
+++ src/main/scala/cbsq/avc/codec-countingbytes1.scala
@@ -0,0 +1,190 @@
+package cbsq.avc
+
+
+
+
+
+
+
+
+
+lazy val countingByteCodec = {
+   
+   import bdIoUtil.drainAndWait
+
+   import bdUtilCct.{CTasking, CTasking1 }
+   
+   trait C extends
+      AnyRef
+      with MediaCodecOverview
+      with CTasking1
+   { c =>
+
+      final val encodedFormMimeType = "application/x-scalavfilter-bytecountingtransmuxer"
+
+      val mediaKind: EMediaKind.Mix.type =
+         EMediaKind.Mix
+
+      /* we deliberately choose `U_R_I`, not `U_R_L`. */
+
+      type CharacteristicFd
+         >: java.io.InputStream | java.nio.channels.SeekableByteChannel | java.io.Reader
+         <: java.io.InputStream | java.nio.channels.SeekableByteChannel | java.io.Reader
+
+      extension [A <: Args](a: A)
+         def withSpecString(s: SpecString)(using ArgsDerivCompletibility[A, a.type, ardtDrvWithSpecString.type] ) =
+            XTaskTemplate(implStart = () => startForUrl(new java.net.URI(s) ) )
+
+      extension [A <: Args](a: A) {
+         
+         def forUrl(s: java.net.URI | java.nio.file.Path)(using ArgsDerivCompletibility[A, a.type, ardtForUrl.type] ) =
+            XTaskTemplate(implStart = () => startForUrl(s) )
+
+         def forOpenedStream(s: CharacteristicFd )(using ArgsDerivCompletibility[A, a.type, ardtForOpenedStream.type] ) =
+            XTaskTemplate(implStart = () => startForOpenedStream(s,  propagateClosedown = s.close _) )
+
+      }
+
+      def startForUrl(s0: java.net.URI | java.nio.file.Path ): Instance =
+         val s = (
+            ((s0 match {
+               
+               case s0 : java.net.URI => 
+                  s0
+               case s0 : java.nio.file.Path => 
+                  s0.toUri()
+
+            }) : java.net.URI )
+            .toURL().openStream()
+         )
+         startForOpenedStream(s, propagateClosedown = s.close _ )
+
+      def startForOpenedStream(s: CharacteristicFd, propagateClosedown: () => Unit ): Instance =
+         import concurrent.ExecutionContext.Implicits.*
+         lazy val ppc1 = {
+            propagateClosedown()
+         }
+         def main() = {
+            // private
+            def logg(u: Unit = (), msg: => String): Unit =
+               sys.process.stderr
+               .println("[StreamByteCountingDemuxer] : " + msg )
+            try {
+               logg(msg = "[bytecounting-demux] applying")
+               bdIoUtil.countAndWait(s)
+               logg(msg = "[bytecounting-demux] done")
+            } finally {
+               ppc1
+            }
+         }
+         val asFuture = (
+            concurrent.Future[Unit]({
+               main()
+            })
+         )
+         new 
+         AnyRef 
+         with java.io.Closeable 
+         with bdUtilCct.KTask[Unit]
+         {
+
+            def close(): Unit = {
+               ppc1
+            }
+
+            val asFuture1 = asFuture
+            
+         }
+
+   }
+
+   {
+      new
+         AnyRef
+         with Selectable /* https://docs.scala-lang.org/scala3/reference/changed-features/structural-types.html */
+         with C
+   }
+
+}
+
+@main
+def runCountingBytesDemuxTest(args: String* ): Unit = {
+   import concurrent.ExecutionContext.Implicits.*
+   import concurrent.duration.*
+   class XIOE extends java.io.IOException("not supported")
+   val throwstream = {
+      val s = java.io.InputStream.nullInputStream()
+      s.close()
+      s
+   }
+
+   val dc = (
+      countingByteCodec
+   )
+
+   util.Using.resource((
+      dc.spawn((
+         dc.argsInitially
+         .forOpenedStream(new java.io.ByteArrayInputStream(new Array[Byte](0x100 ) ) )
+      ) )
+   ))(task => {
+         concurrent.Await.result(task.asFuture1, 20.minutes)
+   })
+   util.Using.resource((
+      dc.spawn((
+         dc.argsInitially
+         // .asReset
+         // .asReset
+         // .asReset
+      ) )
+   ))(task => {
+         concurrent.Await.result(task.asFuture1, 20.minutes)
+   })
+   util.Using.resource((
+      dc.spawn((
+         dc.argsInitially
+         .forOpenedStream(new java.io.ByteArrayInputStream(new Array[Byte](0x1000 ) ) )
+      ) )
+   ))(task => {
+         concurrent.Await.result(task.asFuture1, 20.minutes)
+   })
+   {
+      util.Try[Unit]({
+         util.Using.resource({
+            dc.spawn((
+               dc.argsInitially
+               .forOpenedStream((
+                  throwstream
+               ))
+            ) )
+         })(task => {
+               concurrent.Await.result(task.asFuture1, 20.minutes)
+         })
+      })
+      .recover({
+
+         case z: InterruptedException =>
+            z.printStackTrace() 
+            
+         case z: XIOE => 
+            new Exception("caught intentional exception", z)
+            .printStackTrace()
+
+         case z: Exception =>
+            z.printStackTrace() 
+
+      })
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/scala/cbsq/avc/nullcodec1.scala
@@ -0,0 +1,319 @@
+package cbsq.avc
+
+
+
+
+
+
+
+
+
+
+
+lazy
+val blankingDemuxer = (
+   newBlankingDemuxer()
+)
+
+object bdUtilCct {
+
+   /**
+    * 
+    * this `trait` 
+    * defines exactly `start(): R`
+    * 
+    */
+   trait KStartible[+R] {
+
+      def start(): R
+
+   }
+
+   /**
+    * 
+    * this `trait` 
+    * defines exactly 
+    * [[java.io.Closeable `close(): Unit`]] and *asFuture*
+    * 
+    * @tparam R  the return-value
+    * 
+    */
+   @deprecated("experimental")
+   trait KTask[+R] extends
+      AnyRef
+      with java.io.Closeable
+   {
+      
+      /**
+       * 
+       * terminates the task.
+       * idempotent.
+       * 
+       * note that there's `asFuture1: Future[R]`, meaning that
+       * certain "resources" will need to be left allocated.
+       * 
+       */
+      override
+      def close(): Unit 
+
+      /**
+       * 
+       * a/the `Future` proxying the task's return-value
+       * 
+       */
+      val asFuture1: concurrent.Future[R]
+
+   }
+
+   @deprecated("experimental")
+   def newPromisePair[R]()(using util.NotGiven[R <:< Nothing]) = {
+      val r1 = (
+         concurrent.Promise[R]
+      )
+      (r1.completeWith _, r1.future)
+   }
+
+   trait CTasking extends
+      AnyRef
+      // with Selectable
+      with MediaDeviceOverview /* Metals */
+      // with MediaCodecOverview
+      // with SAbbaAndArgisWereToi
+   { c: MediaDeviceOverview =>
+
+      override
+      inline
+      def spawn(a: Args ): Instance =
+         def logg(u: Unit = (), msg: => String): Unit =
+            sys.process.stderr
+            .println("[CTasking.spawn] : " + msg )
+               
+         logg(msg = s"initialising the task ($a)") 
+
+         ((
+            a.start()
+         ), (
+            logg(msg = s"done init the task ($a)")
+         ))._1
+
+      override
+      type Args
+         <: KStartible[? <: Instance]
+
+      override
+      type Instance
+         <: java.io.Closeable & bdUtilCct.KTask[?]
+
+      override
+      val argsInitially: Args
+
+   }
+
+   // protected
+   class XTaskTemplate[+This, +R <: Any](
+
+      implStart : () => R ,
+
+   ) extends
+   AnyRef
+   with KStartible[R]
+   {
+
+      override
+      inline def start(): R =
+         implStart()
+
+   }
+
+   @deprecated("experimental")
+   trait CTasking1 extends
+      AnyRef
+      // with Selectable
+      with MediaDeviceOverview /* Metals */
+      // with MediaCodecOverview
+      with CTasking
+      // with SAbbaAndArgisWereToi
+   { c: MediaDeviceOverview =>
+
+      override
+      type Args
+         >: XTaskTemplate[? , Instance ]
+         <: XTaskTemplate[? , Instance ]
+
+      override
+      val argsInitially: Args =
+         noopInstanceTemplate
+
+      override
+      type Instance
+         >: java.io.Closeable & bdUtilCct.KTask[?]
+         <: java.io.Closeable & bdUtilCct.KTask[?]
+
+      case object noopInstanceTemplate
+      extends XTaskTemplate(implStart = () => {
+         case object noOpTask
+         extends 
+         AnyRef 
+         with java.io.Closeable
+         with bdUtilCct.KTask[Unit]
+         {
+
+            private[noOpTask] 
+            val alreadyResolved1 = concurrent.Future.unit
+
+            def close(): Unit = {}
+
+            val asFuture1 = alreadyResolved1
+
+         }
+         noOpTask
+      })
+
+      export bdUtilCct.XTaskTemplate
+
+   }
+
+}
+
+object bdIoUtil {
+   
+   import java.io.{IOError, IOException}
+   import java.nio.channels.Channels
+   import java.io.{InputStream, BufferedInputStream, ByteArrayInputStream }
+   import java.io.{Reader     , BufferedReader     , CharArrayReader      }
+   import java.io.OutputStream.nullOutputStream
+   import java.io.Writer.nullWriter
+
+   export ioNullsinkings.drainAndWait
+
+   export ioNullsinkings.countAndWait
+
+   export ioNullsinkings.newByteCountingOutputStream
+
+   export ioNullsinkings.BCOS
+
+}
+
+@deprecated
+def newBlankingDemuxer() =
+   //
+
+   import bdIoUtil.drainAndWait
+
+   import bdUtilCct.{CTasking, CTasking1 }
+
+   trait C extends
+      AnyRef
+      // with Selectable
+      with MediaCodecOverview
+      with CTasking1
+      // with SAbbaAndArgisWereToi
+   { c =>
+
+      final val encodedFormMimeType = "application/x-scalavfilter-discardingdemuxing"
+
+      val mediaKind: EMediaKind.Mix.type =
+         EMediaKind.Mix
+
+      type CharacteristicFd
+         >: java.io.InputStream | java.nio.channels.SeekableByteChannel | java.io.Reader
+         <: java.io.InputStream | java.nio.channels.SeekableByteChannel | java.io.Reader
+
+      extension [A <: Args](a: A)
+         def withSpecString(s: SpecString)(using ArgsDerivCompletibility[A, a.type, ardtDrvWithSpecString.type] ) =
+            noopInstanceTemplate
+
+      extension [A <: Args](a: A)
+
+         def forUrl(s: java.net.URI | java.nio.file.Path)(using ArgsDerivCompletibility[A, a.type, ardtForUrl.type] ) =
+            noopInstanceTemplate
+
+         def forOpenedStream(s: CharacteristicFd )(using ArgsDerivCompletibility[A, a.type, ardtForOpenedStream.type] ) =
+            ForOpenedStream(s)
+
+      private
+      sealed
+      class ForOpenedStream(s: CharacteristicFd )
+      extends
+      XTaskTemplate(implStart = () => {
+         import concurrent.ExecutionContext.Implicits.global
+         
+         // private
+         def logg(u: Unit = (), msg: => String): Unit =
+            sys.process.stderr
+            .println("[StreamBlankingDemuxTask] : " + msg )
+
+         var shallPropagateClosedown: Boolean =
+            true
+
+         val (resolve, asFuture) = (
+            bdUtilCct.newPromisePair[Unit]()
+         )
+
+         logg(msg = s"starting the background drain-thread ($s)")
+         concurrent.Future.unit
+         .map(_ => {
+            drainAndWait(s)
+            logg(msg = s"successfully drained all from ($s)")
+         })
+         .transform(finally1 => util.Try((
+            resolve((
+               concurrent.Future.fromTry(finally1)
+            ) )
+         )) )
+
+         object forOpenedStreamTaskHandle
+         extends 
+         AnyRef 
+         with java.io.Closeable 
+         with bdUtilCct.KTask[Unit]
+         {
+
+            def close(): Unit = {}
+
+            val asFuture1 = asFuture
+
+         }
+         forOpenedStreamTaskHandle
+      })
+
+   }
+
+   {
+      new
+         AnyRef
+         with Selectable /* https://docs.scala-lang.org/scala3/reference/changed-features/structural-types.html */
+         with C
+   }
+
+   // ()
+   
+@main
+def runBlankingDemuxingDemo(): Unit = {
+   val dc = (
+      blankingDemuxer
+   )
+   dc.spawn((
+      dc.argsInitially
+      .forOpenedStream(new java.io.ByteArrayInputStream(Array.emptyByteArray ) )
+   ) )
+   dc.spawn((
+      dc.argsInitially
+      // .asReset
+      // .asReset
+      // .asReset
+   ) )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
