de070b9a4b6f67b8eeb0a40f64f81de9c0044f5e
me <meh@meh>
2023-05-30 18:21:00 +0700

    `avc/upstream/util.scala` - add `GrowableSeq`

--- src/main/scala/cbsq/avc/upstream/util.scala
+++ src/main/scala/cbsq/avc/upstream/util.scala
@@ -26,6 +26,71 @@ export cbsq.avc.LateBoundValue
 
 
 
+trait GrowableSeq[E] extends
+   AnyRef
+   with collection.Seq[E]
+   // with collection.SeqOps[E, GrowableSeq, GrowableSeq[E] ]
+{
+   
+   def prepend(            e: E) : this.type
+   def insert (index: Int, e: E) : Unit
+   def addOne (            e: E) : this.type
+
+}
+object GrowableSeq
+{
+
+   def nextInstance[E] = {
+      newBy[E]()
+   }
+
+   def newBy[E](): (
+      AnyRef
+      & collection.Seq[E]
+      & collection.IndexedSeq[E]
+      & GrowableSeq[E]
+      
+   ) = {
+      val backing = {
+         collection.mutable.ArrayBuffer.empty[E]
+      }
+      new 
+      collection.AbstractSeq[E]
+      with collection.IndexedSeq[E]
+      with GrowableSeq[E]
+      {
+         
+         export backing.length
+         
+         export backing.apply
+         
+         // export backing.iterator
+         
+         export backing.insert
+         export backing.insertAll
+
+         override def prepend(e: E) = { backing prepend e ; this }
+         override def addOne (e: E) = { backing addOne  e ; this }
+         
+         override def knownSize = super.knownSize
+
+         val genericMigrativelySuggestiveSentence = {
+            s"please switch to 'collection.mutable.ArrayBuffer' directly."
+         }
+         
+
+      }
+   }
+
+}
+
+
+
+
+
+
+
+
 
 
 
