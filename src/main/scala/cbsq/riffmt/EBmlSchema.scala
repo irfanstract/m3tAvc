a59622bdb28ab128ffae9036413cd1434362252b
me <meh@meh>
2023-06-09 16:16:23 +0700

    `EBmlSchema.scala` - generic utils - add `Eagerness`

--- src/main/scala/cbsq/riffmt/EBmlSchema.scala
+++ src/main/scala/cbsq/riffmt/EBmlSchema.scala
@@ -1680,6 +1680,45 @@ object ebmsGenericUtils extends
 
       export cbsq.riffmt.ebmls.Lazy
  
+      sealed
+      abstract class Eagerness
+      {
+
+         /**
+          * either `0` when lazy, or `1` when eager
+          */
+         val floatValue : Double
+
+         val characteristicSeqFactory : collection.SeqFactory[Seq]
+
+      }
+      object Eagerness
+      {
+
+         case object toBeLazy extends Eagerness
+         {
+
+            final val floatValue = 0
+
+            final val characteristicSeqFactory = {
+               LazyList
+            }
+
+         }
+         
+         case object toBeEager extends Eagerness
+         {
+
+            final val floatValue = 1
+
+            val characteristicSeqFactory = {
+               IndexedSeq
+            }
+
+         }
+         
+      }
+
       extension (v: String) {
 
          // TODO
