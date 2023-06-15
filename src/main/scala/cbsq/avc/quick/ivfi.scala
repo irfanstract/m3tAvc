7e6852f05911d8818942990e1badfa6dbf3fa78f
me <meh@meh>
2023-06-15 11:36:38 +0700

    `cbsq.avc.quick.openInteractiveFrameIterativeUiImpl` - apply these changes

--- src/main/scala/cbsq/avc/quick/ivfi.scala
+++ src/main/scala/cbsq/avc/quick/ivfi.scala
@@ -100,54 +100,31 @@ extends AnyRef with java.io.Closeable
       
       import generalImpl.eofEdNessState
 
-      val f = new swing.JFrame
-      f setDefaultCloseOperation swing.WindowConstants.DO_NOTHING_ON_CLOSE
-      f addWindowListener {
-         new awt.event.WindowAdapter {
-            import awt.event.WindowEvent
-            override def windowClosing(e: WindowEvent | Null): Unit = {
-               runCloseButtonAction()
-            }
-         }
-      }
-      f setTitle "decoded frames"
-      f setIconImage {
-         import awt.image.*
-         new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB )
-      }
-
-      f setContentPane {
-         val p = new swing.JPanel(new awt.GridLayout )
+      val fSet = {
+      new xSwing.spawnNewJFrame(title = "decoded frames", contentPane = {
+         val p = xSwing.newJPanel(new awt.GridLayout )
          p add {
-            val p = new swing.JPanel(new awt.FlowLayout )
+            val p = xSwing.newInlineSequencePanel()
             p add {
-               val b = new swing.JTextArea(tStampInfoDoc)
-               b.setEditable(false)
-               b
+               xSwing renderTextDocDisplay(tStampInfoDoc)
             }
             p add {
-               val b = new swing.JTextArea(eofEdNessLabel)
-               b.setEditable(false)
-               b
+               xSwing renderTextDocDisplay(eofEdNessLabel)
             }
             p add {
-               new swing.JButton({
-                  (new swing.AbstractAction("next") {
-                     override
-                     def actionPerformed(e: awt.event.ActionEvent) = {
-                        invokeSrcNext()
-                        refresh()
-                     }
-
-                  }) : swing.Action
-               })
+               xSwing renderButton {
+                  xSwing renderAbstractAction(label = "next", callback = e => {
+                     invokeSrcNext()
+                     refresh()
+                  } )
+               }
             }
             p
          }
          p
+      })
       }
-
-      awt.EventQueue.invokeLater(() => { f.setSize(800, 400 ) ; f.setVisible(true) } )
+      export fSet.f
 
       //
       
@@ -157,17 +134,7 @@ extends AnyRef with java.io.Closeable
          f.dispose()
       }
       
-      val wncQ = {
-         cbsq.avc.LateBoundValue.newInstance[Unit]
-      }
-      f addWindowListener {
-         new awt.event.WindowAdapter {
-            import awt.event.WindowEvent
-            override def windowClosed(e: WindowEvent | Null): Unit = {
-               wncQ.tryComplete(util.Success {} )
-            }
-         }
-      }
+      export fSet.{join as join1 }
 
    }
 
@@ -187,7 +154,7 @@ extends AnyRef with java.io.Closeable
     * 
     */
    def join(): Unit = {
-      uiImpl.wncQ.getValue()
+      uiImpl.join1()
    }
 
 }
