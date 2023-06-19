c8baede23d3f92ea30935351bb4fe5e668550a01
me <meh@meh>
2023-06-19 14:05:24 +0700

    apply these changes

--- src/main/scala/avcframewrk/forms/impl/javaswing/allinterfaces.scala
+++ src/main/scala/avcframewrk/forms/impl/javaswing/allinterfaces.scala
@@ -10,6 +10,8 @@ package avcframewrk.forms.impl.javaswing
 
 import avcframewrk.util.forms.pure.*
 
+import avcframewrk.forms.javaswing.*
+
 
 
 
@@ -515,6 +517,7 @@ trait OmiAll[R] extends
 AnyRef
 with DefinesDoRenderButtonA1[javax.swing.Action, R ]
 with XWithNjp[R ]
+with DefinesGetNewPlainOrStyledTextDoc[Any]
 {
 
    export abstractActionFactory.lcafP.{renderButton as renderAbstractAction }
@@ -525,62 +528,6 @@ with XWithNjp[R ]
       initiallySelected : Boolean ,
 
    ) : javax.swing.ButtonModel
-
-   /**
-    * 
-    * returns
-    * a new/independent `Document`
-    * storing plain, unformatted text (MIME Type: `text/plain` ) (hence, UTF) doc.
-    * 
-    */
-   def newPlainTextDocument(
-      //
-
-   ) : javax.swing.text.Document = {
-
-      new javax.swing.text.PlainDocument
-   }
-
-   /**
-    * 
-    * returns
-    * a new/independent `StyledDocument`.
-    * 
-    * avoid making any assumption about the returned `Document`'s actual `class` -
-    * if it's necessary for it to be `HTMLDocument` then
-    * use the following code instead.
-    * 
-    * ```
-    * val doc = {
-    * 
-    *    import javax.swing
-    * 
-    *    val edp = new swing.JEditorPane()
-    * 
-    *    &#x2F;**
-    *     * switch the EditorKit to HTMLEditorKit, so that
-    *     * subsequently `setText` would  parse as HTML code/snippet ;
-    *     * *&#x2F;
-    *    edp setContentType("text/html")
-    * 
-    *    &#x2F;**
-    *     * already invoked switch the EditorKit to HTMLEditorKit, so now
-    *     * invoke `setText` with this HTML code/snippet ;
-    *     * *&#x2F;
-    *    edp setText("&lt;html> ... &lt;body> &lt;p> Paragraph 1 &lt;/p> ... ... ")
-    * 
-    *    (edp.getDocument() ) asInstanceOf[swing.text.html.HTMLDocument ]
-    * } : swing.text.html.HTMLDocument
-    * ```
-    * 
-    */
-   def newStyledTextDocument(
-      //
-
-   ) : javax.swing.text.StyledDocument = {
-
-      new javax.swing.text.html.HTMLDocument
-   }
 
    def newThumbnailsLayout() : Njp[java.awt.LayoutManager]
 
