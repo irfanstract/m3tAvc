b55b3a8d86dfcfb9d4b7c1d1aa551da3c6c199a4
me <meh@meh>
2023-06-21 16:31:41 +0700

    `avcframewrk.forms.impl.javaswing` - bugfix the failure of the returned *JPanel-like*s to acknowledge late(r) *add-call*s, and add these demo-suite(s) for it/those

---
+++ src/main/scala/avcframewrk/forms/demo/javaswing/multi-window-sync.scala
@@ -0,0 +1,138 @@
+package avcframewrk.forms.demo.javaswing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@main
+def runMultiWindowSyncDemo() : Unit = {
+
+   runMultiWindowSyncDemoImpl()
+}
+
+class runMultiWindowSyncDemoImpl(
+   //
+
+)
+(using 
+   // givens
+
+   private
+   val xSwing : avcframewrk.forms.impl.javaswing.allInterfacesGivens.main.type ,
+
+)
+extends
+AnyRef
+{
+
+   import cbsq.avc.newXResourcesManager
+   
+   import java.awt
+   import javax.swing
+
+   import concurrent.ExecutionContext.Implicits.global
+
+   //
+
+   import avcframewrk.forms.javaswing.actionObjUtil.setIcon
+
+   import language.unsafeNulls
+
+   //
+
+   val cd = xSwing.newInlineSequencePanel()
+
+   def spawnOutlet(displayTitle : String ): xSwing.spawnNewJFrame = {
+      ;
+      
+      new xSwing.spawnNewJFrame(title = displayTitle, contentPane = {
+         
+         val c = xSwing.newFourAsidesContentPanel()
+
+         c add cd
+
+         val uNextBtnAction = {
+
+               xSwing renderAbstractAction(label = "next", callback = e => (
+                  //
+
+                  runUNextBtnAction()
+                  
+               ) )
+         }
+
+         val spawnNewOutletBtnAction = {
+
+               xSwing renderAbstractAction(label = s"spawn new outlet", callback = e => (
+                  //
+
+                  spawnOutlet(displayTitle = s"extra window")
+                  
+               ) )
+         }
+
+         c addOne({
+
+            val btnsPanel = xSwing.newInlineSequencePanel()
+
+            btnsPanel add { xSwing renderButton(uNextBtnAction ) }
+
+            btnsPanel add { xSwing renderButton(spawnNewOutletBtnAction ) }
+
+            btnsPanel
+
+         } , awt.BorderLayout.PAGE_END )
+         
+         c
+      } )
+   }
+   
+   for (i <- Range(0, 2) ) {
+      
+      spawnOutlet(displayTitle = s"frame $i ;" )
+
+   }
+
+   def runUNextBtnAction(): Unit = {
+
+      val dateStr = {
+         java.time.LocalDateTime.now()
+         .toString()
+      }
+
+      cd add {
+         xSwing renderButton {
+            xSwing renderAbstractAction(label = s"(date: $dateStr)", callback = { case _ => } )
+         }
+      }
+      
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
