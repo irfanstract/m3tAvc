56b5cb8d4931b2947ef9aa504054d682f673cd47
me <meh@meh>
2023-06-21 08:32:36 +0700

    `avcframewrk.forms.impl.javaswing/jPanelOps.scala` - remove the disabled def `XWithNjp`

--- src/main/scala/avcframewrk/forms/impl/javaswing/jPanelOps.scala
+++ src/main/scala/avcframewrk/forms/impl/javaswing/jPanelOps.scala
@@ -14,39 +14,6 @@ package avcframewrk.forms.impl.javaswing
 
 
 
-// trait XWithNjp[
-//    +R,
-//    +C <: (
-//       {}
-//       & XwnjpFacWithLayoutManager[R]
-//    ) ,
-
-// ](private[XWithNjp] val xwNjpC : C = (new AnyRef).asInstanceOf[C] )
-// {
-
-//    import xwNjpC.WithLayoutManager
-
-//    import java.awt
-//    import javax.swing
-
-//    summon[WithLayoutManager[?] <:< R ]
-
-//    /**
-//     * 
-//     * a `JPanel` with one main content and four asides
-//     * 
-//     */
-//    // def newFourSidebarHolyGrailLayout
-//    def newFourAsidesContentPanel() : WithLayoutManager[awt.BorderLayout ]
-
-//    def newInlineSequencePanel() : WithLayoutManager[awt.LayoutManager ]
-
-//    def newThumbnailsLayout() : WithLayoutManager[awt.LayoutManager ]
-
-//    def newComparativePanel() : WithLayoutManager[awt.LayoutManager ]
-
-// } /* XWithNjpNew */
-
 trait XWithNjpBase[+R, +RFac <: XwnjpFacBase[R] ](protected val xwnjpFac : RFac = { (new AnyRef with XwnjpFacBase[R] ).asInstanceOf[RFac] } )
 extends 
 AnyRef
