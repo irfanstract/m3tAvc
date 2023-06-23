0aded3c148a43daa088c7a2bd55b95820bcc10f1
me <meh@meh>
2023-06-23 13:23:01 +0700

    `avcframewrk.util.forms.pure.gallery-item-display` - add `DefinesDoRenderPoster` and `DefinesDoRenderDynamicPoster`

--- src/main/scala/avcframewrk/util/forms/pure/gallery-item-display.scala
+++ src/main/scala/avcframewrk/util/forms/pure/gallery-item-display.scala
@@ -16,6 +16,28 @@ package avcframewrk.util.forms.pure
 
 
 
+trait DefinesDoRenderPoster[-A, +R]
+{
+
+   def renderPoster(c: A ): R
+
+}
+
+trait DefinesDoRenderDynamicPoster[-A, +R]
+{
+
+   /**
+    * 
+    * treats `c: A` as a possibly-animating source of a graphical content, and
+    * return
+    * (a markup for)
+    * a display-component for it which will animate accordingly
+    * 
+    */
+   def renderDynamicPoster(c: A ): R
+
+}
+
 
 
 
