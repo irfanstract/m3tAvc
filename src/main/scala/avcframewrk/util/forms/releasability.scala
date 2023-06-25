be9a94b25b9214980bf5a3f8a81e0e92dd1dadd0
me <meh@meh>
2023-06-25 12:53:35 +0700

    `avcframewrk.forms` - add group `Releasable` and `ReleasableCond`

--- src/main/scala/avcframewrk/util/forms/releasability.scala
+++ src/main/scala/avcframewrk/util/forms/releasability.scala
@@ -15,6 +15,46 @@ package avcframewrk.util.forms
 
 
 
+;
+
+trait Releasable extends
+AnyRef
+with java.io.Closeable
+{
+
+   /**
+    * 
+    * invoke the `!Release(this)` semantic.
+    * 
+    */
+   def close() : Unit
+
+}
+
+/**
+ * 
+ * defines `close(...)` expecting exactly an argument
+ * 
+ */
+trait ReleasableCond[-Arg, +R] extends
+AnyRef
+{
+
+   /**
+    * 
+    * invoke the `!Release(this, $Arg)` semantic.
+    * 
+    */
+   def close(arg: Arg) : R
+
+}
+
+;
+
+
+
+
+
 
 
 
