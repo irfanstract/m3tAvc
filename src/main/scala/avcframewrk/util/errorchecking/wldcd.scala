93d6055b66792f851aa000c74b743e124c04be9d
me <meh@meh>
2023-06-26 09:05:23 +0700

    `avcframewrk.util.errorchecking.WildcardTypedInstancing` - add this disabled def `Ef1`

--- src/main/scala/avcframewrk/util/errorchecking/wldcd.scala
+++ src/main/scala/avcframewrk/util/errorchecking/wldcd.scala
@@ -19,6 +19,20 @@ package avcframewrk.util.errorchecking
 object WildcardTypedInstancing
 {
 
+   // case class Ef1[I[_], E0](value : I[E0] ) { type E = E0 }
+   // object Ef1
+   // {
+
+   //    implicit def unpack[I[_], E, Impl <: Ef1[I, E] ](w: Impl ) : w.value.type = w.value
+
+   //    implicit
+   //    def instance[I[_], E0](using I[E0] ): Ef1[I, E0] = {
+
+   //       Ef1[I, E0](summon[I[E0] ] )
+   //    }
+      
+   // }
+   
    trait ::![+C[_] ] {
       type Value
       val valuePallete : C[Value]
