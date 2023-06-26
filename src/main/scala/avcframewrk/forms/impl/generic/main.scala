c4aacf7ff29132c1b92e47647531fd33662f4b54
me <meh@meh>
2023-06-26 09:22:29 +0700

    add `avcframewrk.forms.impl.generic.XAllComponentsList`

--- src/main/scala/avcframewrk/forms/impl/generic/main.scala
+++ src/main/scala/avcframewrk/forms/impl/generic/main.scala
@@ -20,6 +20,40 @@ package avcframewrk.forms.impl.generic
 
 val _ = {}
 
+opaque type XAllComponentsList[R]
+   >: avcframewrk.forms.impl.javaswing.XAllComponentsList[R]
+   <: avcframewrk.forms.impl.javaswing.XAllComponentsList[R]
+   = avcframewrk.forms.impl.javaswing.XAllComponentsList[R]
+
+/* `avcframewrk.forms.impl.generic.XAllComponentsList` */
+object XAllComponentsList
+{
+
+   given defaultInstance : impl.epm.valuePallete.type = {
+
+      { import scala.reflect.Selectable.reflectiveSelectable ; impl.epm.valuePallete }
+   }
+
+   private [XAllComponentsList]
+   object impl {
+
+      val epm = {
+
+         new avcframewrk.util.errorchecking.WildcardTypedInstancing.::![XAllComponentsList ] {
+            
+            opaque type Value
+               = avcframewrk.forms.impl.javaswing.allInterfacesGivens.MainR
+
+            final val valuePallete : XAllComponentsList[Value]
+               = avcframewrk.forms.impl.javaswing.allInterfacesGivens.main
+
+         }
+      }
+
+   }
+
+} /* object `XAllComponentsList` */
+
 ;
 
 
