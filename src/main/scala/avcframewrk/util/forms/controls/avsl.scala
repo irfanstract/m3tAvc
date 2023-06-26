f24a4d6190957c6992cf51f6a877a598ef1db828
me <meh@meh>
2023-06-26 16:03:43 +0700

    `avcframewrk.util.forms.controls.avsl.scala` - apply these changes (primarily adding `DefinesDoAcquireSliderExclusive1` and `DefinesDoAcquireSliderExclusive`)

--- src/main/scala/avcframewrk/util/forms/controls/avsl.scala
+++ src/main/scala/avcframewrk/util/forms/controls/avsl.scala
@@ -17,6 +17,62 @@ package avcframewrk.util.forms.controls
 
 ;
 
+val _ = {}
+
+type DefinesDoAcquireSliderExclusive1[Value] = (
+   DefinesDoAcquireSliderExclusive[Value]
+)
+
+protected
+trait DefinesDoAcquireSliderExclusive[Value] extends
+AnyRef
+{
+
+   /**
+    * 
+    * optionally, depending on the args, block until the resource (ie *the slider*) gets relinquished, and then
+      return an itc *proxying the slider*
+    * 
+    * - will, unless `forceRelease == true`,
+    *   block until the previously-acquired mutex gets released
+    *   ,
+    * - implements `avcframewrk.util.forms.ReleasableCond[Boolean, Boolean]`.
+    *   pass `true` to maintain, or pass `false` to reset ;
+    *   may return `true` or `false` indicating whether the release succeeded
+    * 
+    * 
+    */
+   def acquireSlider(
+      // 
+
+      forceImmediateReset  : Boolean = false ,
+      forceImmediateAccept : Boolean = false ,
+      
+   )(using
+      evidenceOfArgsValidity : (
+         util.NotGiven[{
+            val fir : forceImmediateReset.type
+            val fia : forceImmediateAccept.type
+         } <:< {
+            val fir : true
+            val fia : true
+         } ]
+      ) ,
+   ) : (
+      AnyRef
+      & avcframewrk.util.forms.ReleasableCond[Boolean, Boolean]
+      & AvslReassignableBy[Value]
+   )
+
+   if false then {
+      acquireSlider()
+      // acquireSlider(forceImmediateReset = true, forceImmediateAccept = true )
+   }
+
+}
+
+val _ = {}
+
 type AvslReassignableBy[-Value]
    >: AvslApplyWithExactNewValue[Value, Unit ]
    <: AvslApplyWithExactNewValue[Value, Unit ]
