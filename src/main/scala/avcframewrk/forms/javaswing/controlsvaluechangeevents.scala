5fcdb4dc3e94589a1c2645fbfd7e87a2380cc83c
me <meh@meh>
2023-06-27 18:09:54 +0700

    `avcframewrk.forms.javaswing` - add `extension [Value](s: tsevp.EventIterator[CvcEvent[Value]]) def asAnimatedSwingTextDocument(): swing.text.Document`

--- src/main/scala/avcframewrk/forms/javaswing/controlsvaluechangeevents.scala
+++ src/main/scala/avcframewrk/forms/javaswing/controlsvaluechangeevents.scala
@@ -20,6 +20,32 @@ package avcframewrk.forms.javaswing
 
 val _ = {}
 
+extension [Value](s: avcframewrk.util.EventIterator[CvcEvent[Value ] ] ) {
+
+   /**
+    * 
+    * a `javax.swing.text.Document` to
+    * constantly animate/refresh/update everytime it receives new ValueChangeEvent(s)
+    * 
+    */
+   def asAnimatedSwingTextDocument(): javax.swing.text.Document = {
+
+      import language.unsafeNulls
+      
+      import avcframewrk.forms.javaswing.swingTextDocUtils.setText1
+
+      val d = new javax.swing.text.PlainDocument
+
+      for (value <- s.map(_.newValue) ) do {
+         
+         d setText1 s"value: ${value}; "
+      }
+
+      d
+   }
+
+}
+
 /**
  * 
  * Form Control Value Change Event
