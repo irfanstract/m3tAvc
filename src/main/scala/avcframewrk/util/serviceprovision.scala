f10589e2b4082f1d90f14512b8e9b2a10ea4a7c7
me <meh@meh>
2023-07-10 02:14:00 +0700

    apply these changes

---
+++ src/main/scala/avcframewrk/util/serviceprovision.scala
@@ -0,0 +1,55 @@
+package avcframewrk.util
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
+
+object Implementory {
+
+   //
+
+   // def getItcInstance[I](c: java.lang.Class[I] )
+   // = {
+      
+   //    import language.unsafeNulls
+
+   //    java.util.ServiceLoader.load(c )
+   //    .iterator()
+   //    .next()
+   // }
+   
+}
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
