3f2295a6371099e99b5bede254904432a0ec94ad
me <meh@meh>
2023-07-22 08:24:53 +0700

    [`avcframewrk.util.forms.pure`] init `util.scala`
    ```
    export avcframewrk.util.EventIterator
    export avcframewrk.util.EventIteratorByItemAndDesignation
    export avcframewrk.util.TsevpEventType
    ```

---
+++ src/main/scala/avcframewrk/util/forms/pure/util.scala
@@ -0,0 +1,40 @@
+package avcframewrk.util.forms.pure
+
+val _ = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
+
+export avcframewrk.util.EventIterator
+export avcframewrk.util.EventIteratorByItemAndDesignation
+export avcframewrk.util.TsevpEventType
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
