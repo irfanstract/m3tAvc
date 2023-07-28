dfdacb2c67e8bb949846c767ed13d40a302b2cc9
me <meh@meh>
2023-07-28 10:49:37 +0700

    apply these changes, (1) to *the meta*, primarily for SJS, (2) to `events-functoring.scala`

--- project/project/build1.scala
+++ project/project/build1.scala
@@ -1,3 +1,4 @@
+/* useless -- nothing could be resolved from code in the receiving dir (ie the enclosing dir) */
 
 
 
@@ -11,10 +12,28 @@
 
 
 
-
+@deprecated("useless -- nothing could be resolved from code in the receiving dir (ie the enclosing dir).")
 object PreBuild {
 
    import sbt.*
+
+   // /**
+   //  * 
+   //  * the artifact for the backend plugin (eg SNative, SJS)
+   //  * 
+   //  */
+   // final
+   // lazy val backendPluginArtRef
+   // : ModuleID
+   // = 
+   //    identity[ModuleID](
+
+   //       "org.scala-js" % "sbt-scalajs" % (
+   //          // "1.13.2"
+   //          // "1.0.0"
+   //          "1.13.2"
+   //       )
+   //    )
 
 }
 
