58952a6c54820ca2dafb6dc8c2ade9b68e004d6c
me <meh@meh>
2023-07-28 12:42:50 +0700

    [`project\build1.scala`] add `import sbt.*` and `object vars`
    ```
       import sbt.*
    
       /**
        * config which needs to be manually sync-ed against the correspondents in other file(s)
        */
       object vars
    ```

--- project/build1.scala
+++ project/build1.scala
@@ -14,6 +14,17 @@
 
 object Build {
 
+   import sbt.*
+
+   /**
+    * 
+    * config which needs to be manually sync-ed against the correspondents in other file(s)
+    * 
+    */
+   object vars {
+
+   } /* object vars */
+
 }
 
 
