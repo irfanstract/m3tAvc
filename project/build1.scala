336c79d626b068f3c87194598238001aa6f35841
me <meh@meh>
2023-07-28 12:46:45 +0700

    [`project\build1.scala`] add `val pbgConfig = Configuration.of("XCompilerPluginBc", "plugin->default(compile)")`
    to implement `platform.config.version`
    Bloop stuck to
    looping over `libraryDependencies` and
    filtering to ones whose, in addition to being in/of the expected `organization`, `configuration` "matches" `CompilerPluginConfig`.
    this means that
    there *needs to* be the relevant SN-or-SJS artifact(s) (with)in `libraryDependencies`, having that `Configuration`

--- project/build1.scala
+++ project/build1.scala
@@ -25,6 +25,23 @@ object Build {
 
    } /* object vars */
 
+   /** 
+    * 
+    * to implement `platform.config.version`
+    * Bloop stuck to
+    * looping over `libraryDependencies` and
+    * filtering to ones whose, in addition to being in/of the expected `organization`, `configuration` "matches" `CompilerPluginConfig`.
+    * this means that
+    * there *needs to* be the relevant SN-or-SJS artifact(s) (with)in `libraryDependencies`, having that `Configuration`
+    * 
+    */
+   final
+   lazy val pbgConfig
+   = {
+
+      Configuration.of("XCompilerPluginBc", "plugin->default(compile)")
+   }
+
 }
 
 
