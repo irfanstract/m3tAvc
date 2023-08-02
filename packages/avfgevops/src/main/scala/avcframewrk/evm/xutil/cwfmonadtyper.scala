fe3e79f73bd27ca213c2b48379ad94ea4e747061
me <meh@meh>
2023-08-02 16:59:16 +0700

    apply these changes

--- packages/avfgevops/src/main/scala/avcframewrk/evm/xutil/cwfmonadtyper.scala
+++ packages/avfgevops/src/main/scala/avcframewrk/evm/xutil/cwfmonadtyper.scala
@@ -16,6 +16,36 @@ package avcframewrk.evm.xutil
 type CwfMonadTypeAsMain
    = { type Main[+E] <: collection.WithFilter[E, Main ] }
 
+// /**
+//  * 
+//  * defines `collect` for `definer.Main[?]`.
+//  * 
+//  */
+// case class CwfMonadCollectivity[
+//    +Definer <: Singleton & CwfMonadTypeAsMain ,
+
+// ](definer: Definer )
+// {
+
+//    extension[E0] (s: definer.Main[E0] )
+//    {
+
+//       def collect[E2] (f: PartialFunction[E0, E2] )
+//       : definer.Main[E2]
+//       = {
+
+//          s
+//          .flatMap(e0 => {
+//             f
+//             .andThen[Some[E2] ](e20 => Some(e20) )
+//             .applyOrElse[E0, Option[E2] ](e0, _ => None )
+//          } )
+//       }
+
+//    }
+
+// }
+
 
 
 
