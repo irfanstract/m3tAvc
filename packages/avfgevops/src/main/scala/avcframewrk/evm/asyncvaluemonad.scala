cc9aea1a70722b1052991eb000ef587184f05f8c
me <meh@meh>
2023-08-03 00:22:07 +0700

    [`avcframewrk.evm`] rename `AsyncAlgMonad` into `AsyncAlgebraicMonad` leaving an alias

--- packages/avfgevops/src/main/scala/avcframewrk/evm/asyncvaluemonad.scala
+++ packages/avfgevops/src/main/scala/avcframewrk/evm/asyncvaluemonad.scala
@@ -15,9 +15,12 @@ package avcframewrk.evm
  * a "monad" without any synchronously-inspective ops
  * 
  */
-type AsyncAlgMonad[+E1]
+type AsyncAlgebraicMonad[+E1]
    >: (avcframewrk.evm.xutil.CwfMonadTypeAsMain )#Main[E1]
    <: (avcframewrk.evm.xutil.CwfMonadTypeAsMain )#Main[E1]
+
+type AsyncAlgMonad[E1]
+   = AsyncAlgebraicMonad[E1]
 
 
 
