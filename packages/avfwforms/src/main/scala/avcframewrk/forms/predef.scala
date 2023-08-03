d586242bd09274621ab2598376efd9f7c6a5e596
me <meh@meh>
2023-08-03 12:10:21 +0700

    [`avcframewrk.forms`] [predefs] add `type TL[C <: AnyKind] = C` (which failed to solve the issue)

--- packages/avfwforms/src/main/scala/avcframewrk/forms/predef.scala
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/predef.scala
@@ -15,6 +15,23 @@ package avcframewrk.forms
 
 
 
+/**
+ * 
+ * `C`, supposed to be a type-lmd.
+ * 
+ * ```
+ * type ItsAllocator
+ *    >: LC[ [c &lt;: InstanceOf[Bar] ] =>> (.....) ]
+ *    &lt;: LC[ [c &lt;: InstanceOf[Bar] ] =>> (.....) ]
+ * ```
+ * 
+ * didn't work as supposed to
+ * 
+ */
+@deprecated("this didn't work as expected.")
+type TL[C <: AnyKind]
+   = C
+
 
 
 
