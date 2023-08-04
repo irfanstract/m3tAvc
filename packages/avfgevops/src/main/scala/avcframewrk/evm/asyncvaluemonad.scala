39f4fa931af4ee8b3a9cea0650dd5950a841e762
me <meh@meh>
2023-08-05 06:52:30 +0700

    [`avcframewrk.evm`] add `AsyncAlgebrMonad`
    ```
    type AsyncAlgebrMonad[E1]
       = AsyncAlgebraicMonad[E1]
    ```

--- packages/avfgevops/src/main/scala/avcframewrk/evm/asyncvaluemonad.scala
+++ packages/avfgevops/src/main/scala/avcframewrk/evm/asyncvaluemonad.scala
@@ -19,6 +19,9 @@ type AsyncAlgebraicMonad[+E1]
    >: ({ type Main[+E2] = monix.reactive.Observable[E2] } )#Main[E1]
    <: ({ type Main[+E2] = monix.reactive.Observable[E2] } )#Main[E1]
 
+type AsyncAlgebrMonad[E1]
+   = AsyncAlgebraicMonad[E1]
+
 type AsyncAlgMonad[E1]
    = AsyncAlgebraicMonad[E1]
 
