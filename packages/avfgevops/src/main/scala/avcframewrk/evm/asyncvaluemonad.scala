ed2a20414c097a4cbd22c77a07ae7e7761ccf2e3
me <meh@meh>
2023-08-05 08:21:47 +0700

    [`avcframewrk.evm`] add `newAlgebrPipe(.....)`

--- packages/avfgevops/src/main/scala/avcframewrk/evm/asyncvaluemonad.scala
+++ packages/avfgevops/src/main/scala/avcframewrk/evm/asyncvaluemonad.scala
@@ -25,6 +25,62 @@ type AsyncAlgebrMonad[E1]
 type AsyncAlgMonad[E1]
    = AsyncAlgebraicMonad[E1]
 
+/**
+ * 
+ * a fresh/independent/new pipe (`AsyncAlgebraicMonad`) ;
+ * the first itc being for the producer-side,
+ * the second itc being for the consumer-side
+ * 
+ * *subscriber*s can come later than items emitted, so
+ * it'd be necessary to define the expected behv, by setting `multicastStrategy`
+ * 
+ * 
+ * @param typingFnc a work-around to programmers forgetting to define `E1` explicitly
+ * 
+ * @param multicastStrategy [[monix.reactive.MulticastStrategy]]
+ * 
+ * @param scheduler `monix.execution.Scheduler`
+ * 
+ */
+def newAlgebrPipe[E1](
+   typingFnc: E1 => Unit ,
+
+   multicastStrategy
+   : monix.reactive.MulticastStrategy[E1]
+   ,
+
+   scheduler
+   : monix.execution.Scheduler
+   = monix.execution.Scheduler.Implicits.global
+   ,
+   
+)
+: (
+   /* first, the producer-side itc */
+   monix.reactive.Observer[E1]
+   ,
+
+   /* second, the consumer-side itc */
+   AsyncAlgebraicMonad[E1]
+   ,
+   
+)
+= {
+
+   val core1 = {
+      
+      import monix.{reactive => m }
+
+      given scheduler.type
+         = scheduler
+
+      m.subjects.ConcurrentSubject[E1](multicastStrategy )
+      
+   }
+
+   (core1, core1 )
+}
+
 
 
 
