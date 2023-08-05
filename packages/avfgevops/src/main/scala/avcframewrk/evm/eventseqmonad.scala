75deee1196da02a3fbaa1619af82ee20490506ca
me <meh@meh>
2023-08-05 08:48:07 +0700

    [`avcframewrk.evm`] add `newEventSequencePipe(.....)`

--- packages/avfgevops/src/main/scala/avcframewrk/evm/eventseqmonad.scala
+++ packages/avfgevops/src/main/scala/avcframewrk/evm/eventseqmonad.scala
@@ -45,6 +45,46 @@ def emptyAsyncEventSequenceMonad
    .asInstanceOf[EventSeqMonad[Nothing] ]
 }
 
+/**
+ * 
+ * a fresh/independent/new pipe (`EventSequenceMonad`) ;
+ * the first itc being for the producer-side,
+ * the second itc being for the consumer-side
+ * 
+ * *subscriber*s can come later than items emitted, so
+ * it'd be necessary to define the expected behv, by setting `multicastStrategy`
+ * 
+ * 
+ * @param typingFnc a work-around to programmers forgetting to define `E1` explicitly
+ * 
+ * @param multicastStrategy [[monix.reactive.MulticastStrategy]]
+ * 
+ * @param scheduler `monix.execution.Scheduler`
+ * 
+ */
+def newEventSequencePipe[
+   E1 <: Product ,
+]
+(
+   typingFnc: E1 => Unit ,
+
+   multicastStrategy
+   : monix.reactive.MulticastStrategy[E1]
+   ,
+
+   scheduler
+   : monix.execution.Scheduler
+   = monix.execution.Scheduler.Implicits.global
+   ,
+
+)
+= {
+
+   val (p1, p20) = newAlgebrPipe[E1](typingFnc = identity[E1], multicastStrategy = multicastStrategy )
+
+   (p1, p20)
+}
+
 lazy val _ = {
 
    emptyAsyncEventSequenceMonad
