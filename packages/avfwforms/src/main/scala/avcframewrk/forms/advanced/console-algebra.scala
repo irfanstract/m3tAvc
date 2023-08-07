1f4fee33b145109ad644c8a95f7ebeaec3cf3501
me <meh@meh>
2023-08-07 14:28:57 +0700

    [`avcframewrk.forms`] move `clauseInterweavedActionItemDispatcherImpl` into sub-package `advanced`, leaving alias

---
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/advanced/console-algebra.scala
@@ -0,0 +1,80 @@
+package avcframewrk.forms
+
+package advanced
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@annotation.experimental
+object clauseInterweavedActionItemDispatcherImpl
+{
+
+   /* the language imports (though, for some reason putting em here works) */
+
+   import language.experimental.clauseInterleaving
+
+   /* the library name imports */
+
+   import Promptibility.XDispatchTimePrereqsImpl
+
+   /* the main def */
+
+   trait Main
+   {
+
+      /**
+       * 
+       * .
+       * 
+       * 
+       * @param main the main action item
+       * @param manager the main support for dealing with `main`
+       * 
+       * @param expectedAsynchronicity `LexicalImperativeSynchronicityGiven`
+       * 
+       */
+      def apply
+         (
+            main
+            : (
+               // Product
+               Any
+            )
+            ,
+         )
+         [M >: main.type <: Any ]
+         (using
+            manager: XDispatchTimePrereqsImpl[M]
+            ,
+         )
+         (using
+            expectedAsynchronicity
+            : avcframewrk.forms.math.LexicalImperativeSynchronicityGiven.ByCc[[_] =>> Any ]
+            ,
+         )
+         : (
+            ([R] =>> (
+               expectedAsynchronicity.MainByReturnValue[R]
+            ))
+            [(
+               // TODO wait until the first cand no longer crash the compiler, and switch back to it
+
+               manager.ItsRfdXValue[main.type]
+            ) ]
+
+         ) // r
+
+   }
+
+} // `&&&%!`
