7c1fca64224e91e08a29d54266dc155802b67f03
me <meh@meh>
2023-08-08 08:47:55 +0700

    [`avcalg.`] [`zero-and-concat.scala`] init these four *given*s
    - `Concatenability`
    - `ConcatenabilityK`
    - `CBC`
    - `CBCK`

--- packages/avfwalgeb/src/main/scala/avcalg/zero-and-concat.scala
+++ packages/avfwalgeb/src/main/scala/avcalg/zero-and-concat.scala
@@ -15,6 +15,60 @@ package avcalg
 
 
 
+trait Concatenability[C ](using peer : cats.Semigroup[C] )
+extends
+AnyRef
+{
+
+   transparent inline /* needs to be `transparent` to avoid the need for explicit (re)ovd/def */
+   def reverse
+   = peer.reverse
+
+}
+
+trait ConcatenabilityK[C[_] ](using peer : cats.SemigroupK[C] )
+extends
+AnyRef
+{
+
+   transparent inline /* needs to be `transparent` to avoid the need for explicit (re)ovd/def */
+   def reverse
+   = peer.reverse
+
+}
+
+given [C    ](using peer : cats.Semigroup[C]  ) : Concatenability[C] with {}
+
+given [C[_] ](using peer : cats.SemigroupK[C] ) : ConcatenabilityK[C] with {}
+
+trait CBC[C ](using peer : cats.Monoid[C] )
+extends
+AnyRef
+with Concatenability[C ]
+{
+
+   transparent inline /* needs to be `transparent` to avoid the need for explicit (re)ovd/def */
+   def empty
+   = peer.empty
+
+}
+
+trait CBCK[C[_] ](using peer : cats.MonoidK[C] )
+extends
+AnyRef
+with ConcatenabilityK[C ]
+{
+
+   transparent inline /* needs to be `transparent` to avoid the need for explicit (re)ovd/def */
+   def empty[E]
+   = peer.empty[E]
+
+}
+
+given [C    ](using peer : cats.Monoid[C]  ) : CBC[C] with {}
+
+given [C[_] ](using peer : cats.MonoidK[C]  ) : CBCK[C] with {}
+
 
 
 
