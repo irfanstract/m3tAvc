94ceb7e22233fb272192ccfd5144c2f872f7b42c
me <meh@meh>
2023-08-08 08:51:08 +0700

    [`avcalg.`] init `itemwise.scala`

---
+++ packages/avfwalgeb/src/main/scala/avcalg/itemwise.scala
@@ -0,0 +1,95 @@
+package avcalg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export cats.{Functor   =>       ItemWiseTransformability    }
+// export cats.{Functor   =>    ElementWiseTransformability    }
+// export cats.{Monad     =>       ItemWiseUnfoldability       }
+// export cats.{Monad     =>    ElementWiseUnfoldability       }
+// export cats.{Monad     =>           ListUnfoldability       }
+// export cats.{Monad     =>             LsUnfoldability       }
+
+trait ItemWiseTransformability[CC[_] ](using catsFunctorCc : cats.Functor[CC] )
+extends
+AnyRef
+{
+
+}
+
+trait ItemWiseUnfoldability[CC[_] ](using catsMonadCc : cats.Monad[CC] )
+extends
+AnyRef
+with ItemWiseTransformability[CC]
+{
+
+}
+
+type ElementWiseTransformability[CC[_] ]
+   = ItemWiseTransformability[CC ]
+
+type ElementWiseUnfoldability[CC[_] ]
+   = ItemWiseUnfoldability[CC ]
+
+given [CC[_] ](using cats.Functor[CC] )
+: AnyRef with ItemWiseTransformability[CC ] with {}
+
+given [CC[_] ](using cats.Monad[CC] )
+: AnyRef with ItemWiseUnfoldability[CC] with {}
+
+// export cats.{Foldable      =>       LsFoldability        }
+// export cats.{Foldable      =>     ListFoldability        }
+// export cats.{Reducible     =>       LsReducibility       }
+// export cats.{Reducible     =>     ListReducibility       }
+
+trait ListFoldability[CC[_] ](using catsFoldableCc : cats.Foldable[CC] )
+extends
+AnyRef
+{
+
+}
+
+trait ListReducibility[CC[_] ](using catsReduxCc : cats.Reducible[CC] )
+extends
+AnyRef
+{
+
+}
+
+given [CC[_] ](using cats.Foldable[CC] )
+: AnyRef with ListFoldability[CC ] with {}
+
+given [CC[_] ](using cats.Reducible[CC] )
+: AnyRef with ListReducibility[CC ] with {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+val _ = {}
