d4df01eb9fd0baffb2049de43e69486c498ce16e
me <meh@meh>
2023-08-08 09:59:43 +0700

    [`avcframewrk.evm.xutil`] [`CwfMonadTypeAsMain`] add this `@deprecated("rather than building on inheritance, ... .")`

--- packages/avfgevops/src/main/scala/avcframewrk/evm/xutil/cwfmonadtyper.scala
+++ packages/avfgevops/src/main/scala/avcframewrk/evm/xutil/cwfmonadtyper.scala
@@ -13,6 +13,7 @@ package avcframewrk.evm.xutil
  * `{ type Main[+E] <: collection.WithFilter[E, Main ] }`
  * 
  */
+@deprecated("rather than building on inheritance, consider extension-method(s) or implicit-view(s) instead.")
 type CwfMonadTypeAsMain
    = { type Main[+E] <: collection.WithFilter[E, Main ] }
 
