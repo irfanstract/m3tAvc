dc82867c8fe6cc0298ae50778f1cdba197bb9342
me <meh@meh>
2023-08-08 19:56:28 +0700

    Revert "[`avcframewrk.evm.xutil`] [`CwfMonadTypeAsMain`] add this `@deprecated("rather than building on inheritance, ... .")`"
    This reverts commit d4df01eb9fd0baffb2049de43e69486c498ce16e.

--- packages/avfgevops/src/main/scala/avcframewrk/evm/xutil/cwfmonadtyper.scala
+++ packages/avfgevops/src/main/scala/avcframewrk/evm/xutil/cwfmonadtyper.scala
@@ -13,7 +13,6 @@ package avcframewrk.evm.xutil
  * `{ type Main[+E] <: collection.WithFilter[E, Main ] }`
  * 
  */
-@deprecated("rather than building on inheritance, consider extension-method(s) or implicit-view(s) instead.")
 type CwfMonadTypeAsMain
    = { type Main[+E] <: collection.WithFilter[E, Main ] }
 
