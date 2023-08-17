66daac8288f2b52c6a048247cac07ec80618bf48
me <meh@meh>
2023-08-17 13:55:00 +0700

    [bundle `avfwforms` test] [`PromptibilityItcsTypeDefinitiveTest`] add this another "this basic example compiles" case

--- packages/avfwforms/src/test/scala/promptibility-itc-1.scala
+++ packages/avfwforms/src/test/scala/promptibility-itc-1.scala
@@ -46,6 +46,33 @@ org.scalatest.funsuite.AnyFunSuite
 
    }
 
+   test("this basic example 2 compiles and completes") {
+
+      //
+
+      def showAlert
+         [
+            A1 <: Product
+            ,
+
+         ]
+         (m: A1 )
+         (using Promptibility.XDispatcher.ByNoSpec )
+         (using Question.HeadlineExtractor[m.type], Question.AcceptableResponseFormatDescExtractorAlgebraic[A1] )
+      = {
+
+         implicit val fgc
+         = avcframewrk.forms.math.LexicalImperativeSynchronicityGiven.forByNameAlikeSem
+
+         (summon[Promptibility.XDispatcher.ByNoSpec ] )
+         .apply(m)
+
+      }
+
+      assert(1 == 1 )
+
+   }
+
 }
 
 
