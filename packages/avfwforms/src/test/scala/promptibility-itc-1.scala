e2dade6a61935e6971344f7488d7380812412773
me <meh@meh>
2023-08-17 13:42:56 +0700

    [bundle `avfwforms` test] [`PromptibilityItcsTypeDefinitiveTest`] impl

---
+++ packages/avfwforms/src/test/scala/promptibility-itc-1.scala
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+import avcframewrk.forms.*
+
+
+
+
+
+
+
+class PromptibilityItcsTypeDefinitiveTest
+extends
+org.scalatest.funsuite.AnyFunSuite
+{
+
+   test("this basic example compiles and completes") {
+
+      //
+
+      def showAlert
+         [
+            A1 <: Product
+            ,
+
+         ]
+         (m: A1 )
+         (using Promptibility.XDispatcher.ByNoSpec )
+         (using Question.HeadlineExtractor[m.type], Question.AcceptableResponseFormatDescExtractorAlgebraic[A1] )
+         (using advanced.clauseInterweavedActionItemDispatcherImpl.Main.LexicalAsynchronicity )
+      = {
+
+         (summon[Promptibility.XDispatcher.ByNoSpec ] )
+         .apply(m)
+
+      }
+
+      assert(1 == 1 )
+
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
