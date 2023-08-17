e653a2fb4d240c9ecefa97358a95566f4b7b89bc
me <meh@meh>
2023-08-17 10:06:23 +0700

    [`avcframewrk.forms`] introduce `clauseInterweavedActionItemDispatcherImpl.`, together with member(s) `Main` basic impl
    (the second time, after deletion of the whole file)

---
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/advanced/console-algebra.scala
@@ -0,0 +1,93 @@
+package avcframewrk.forms
+
+package advanced
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+object clauseInterweavedActionItemDispatcherImpl
+{
+
+   /* the language imports (though, for some reason putting em here works) */
+
+   /* the library name imports */
+
+   import Question.XSummedAllView
+
+   /* the main def */
+
+   trait Main
+   {
+
+      import Main.LexicalAsynchronicity
+
+      ()
+
+      ()
+
+      /**
+       * 
+       * .
+       * 
+       * 
+       * @param main the main action item
+       * @param expectedAsynchronicity `LexicalImperativeSynchronicityGiven`
+       * 
+       */
+      def apply
+         [
+            A <: Product
+            ,
+         ]
+         (
+            main
+            : (
+               // A
+               XSummedAllView[A ]
+               // Any
+            )
+            ,
+         )
+         (using
+            expectedAsynchronicity
+            : LexicalAsynchronicity
+            ,
+         )
+         : (
+            ({
+               type R = expectedAsynchronicity.MainByReturnValue[RVal]
+               type RVal = main.responseFormat.XValue
+            })
+            #R
+
+         ) // r
+
+   } // Main
+
+   object Main {
+
+      //
+
+      type LexicalAsynchronicity
+         = avcframewrk.forms.math.LexicalImperativeSynchronicityGiven.ByCc[[_] =>> Any ]
+
+      object LexicalAsynchronicity {
+
+         //
+
+      } // LexicalAsynchronicity$
+
+   } // Main$
+
+} // clauseInterweavedActionItemDispatcherImpl$
