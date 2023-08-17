e0f859077c06d3757e59328b72c478d1596ef33f
me <meh@meh>
2023-08-17 12:04:32 +0700

    apply these changes

--- packages/avfwforms/src/test/scala/promptibility-itc.scala
+++ packages/avfwforms/src/test/scala/promptibility-itc.scala
@@ -11,6 +11,8 @@ import avcframewrk.forms.*
 
 
 
+/* see also "SIP 48 -- the 'precise' type-modifier" https://github.com/scala/improvement-proposals/pull/48 */
+
 extension (dispatcher: Promptibility.XDispatcher.ByNoSpec )
 {
 
@@ -23,15 +25,19 @@ extension (dispatcher: Promptibility.XDispatcher.ByNoSpec )
    // : Unit
    = {
 
-      {
+      val r = {
          
          given Question.AcceptableResponseFormatDescExtractorAlgebraic[A1]
          = ???
+
+         // val s = summon[Conversion[A1, Question.XSummedAllView[A1] ] ]
 
          // Question.XSummedAllViewImpl.apply[m.type](m)
 
          dispatcher.apply(m)
       }
+
+      r
 
    }
 
