f17c6c5ed5dce92d24f9db1d15a016264cb831e8
me <meh@meh>
2023-08-19 20:58:27 +0700

    [`packages\main-sjs\.js`] apply these changes

---
+++ packages/main-sjs/.js/vite.config.js
@@ -0,0 +1,21 @@
+
+
+import { defineConfig } from "vite";
+import getScalaJSPlugin from "@scala-js/vite-plugin-scalajs";
+/**
+ * @typedef {import("@scala-js/vite-plugin-scalajs").ScalaJSPluginOptions } ScalaJSPluginOptions
+ */
+
+
+
+
+export default (
+  defineConfig({
+    plugins: [
+    ],
+  })
+) ;
+
+
+
+
