7db1fcaec2077b76eabee284032a0e9683230ac2
me <meh@meh>
2023-08-21 15:57:47 +0700

    [`avcframewrk.forms`] [Templating] [W3E] apply these additions

---
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/w3e/pre/articles.scala
@@ -0,0 +1,37 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e.pre
+
+
+
+
+
+
+
+
+
+
+
+
+trait Articles
+extends
+AnyRef
+{
+
+   type Article
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/w3e/pre/buttons.scala
@@ -0,0 +1,45 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e.pre
+
+
+
+
+
+
+
+
+
+
+
+
+trait Buttons
+extends
+AnyRef
+with Articles
+{
+
+   type Action
+
+   val Action
+   : (
+      AnyRef
+   )
+
+   type ButtonArt
+      <: Article
+
+}
+
+
+
+
+
+
+
+
+
+
+
