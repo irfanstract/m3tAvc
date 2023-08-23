78f5bd98836829a1eeef8d7c663ecb09d6fd1a4b
me <meh@meh>
2023-08-23 13:39:03 +0700

    [`avcframewrk.forms` for JS] apply these additions

---
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/eb.scala
@@ -0,0 +1,352 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e.impl.js
+
+package s1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+trait EbAndReconcilability
+{
+   //
+
+   //
+
+   type ElementBase
+      >: org.scalajs.dom.Element /* `class`, not `trait` */
+      <: org.scalajs.dom.Element /* `class`, not `trait` */
+
+   // TODO
+   trait ToHtml {
+
+      def apply() : String
+
+   }
+
+   object SjsEOps
+   {
+      //
+
+      //
+
+      ;
+
+      // /**
+      //  *
+      //  * Sjs Child Element Class Ops Parent Children Relationship
+      //  *
+      //  */
+      // trait Pcr {
+
+      //    val parent
+      //    : ClassOps
+         
+      //    type parent$
+      //    = parent.type
+
+      //    type Child
+      //    <: ClassInstanceOps
+ 
+      // }
+
+      object ClassInstanceOps
+      {
+
+         //
+
+      }
+
+      trait ClassInstanceOps(
+         //
+      )
+      extends
+      AnyRef
+      with ParentChildRelationship.Cio.ClassInstanceOps[ClassOps, ClassInstanceOps]
+      {
+
+         //
+
+         // val given_Pcr
+         // : Pcr
+         ;
+
+         // val class1
+         // : ClassOps
+
+         type ComparableSpawnedElement
+            >: class1.ComparableSpawnedElement
+            <: class1.ComparableSpawnedElement
+
+         type SpawnedElement
+            <: ComparableSpawnedElement
+
+         val reconciler
+         : (
+            AnyRef
+            & (
+               PerSampleSpawningAndReconcilingOps[ComparableSpawnedElement]
+               & PerSampleSpawningAndReconcilingOps.CtxtualSpawnMethods[ClassInstanceOps.this.SpawnedElement ] 
+            )
+         )
+
+         /**
+          *
+          * .
+          *
+          */
+         export reconciler.spawn
+
+         /**
+          *
+          * in-place update of it.
+          *
+          */
+         export reconciler.updateAndSelf
+
+         /**
+          *
+          * try to generate HTML (it's not always possibly).
+          *
+          * defaults to `spawn().outerHTML`.
+          *
+          */
+         // @deprecated
+         export reconciler.toHTML
+
+         //
+
+      }
+
+      /**
+       * 
+       * in practice,
+       * `spawn()`
+       * tends to behave as the sequence `val e = document.createElement("some-element") ; updateAndSelf(e) ;`,
+       * and
+       * the similar case happens for `toHTML()`
+       * 
+       */
+      trait PerSampleSpawningAndReconcilingOps[
+         CSE  <: ClassOps#ComparableSpawnedElement
+         ,
+      ]
+      extends
+      AnyRef
+      with PerSampleSpawningAndReconcilingOps.CtxtualSpawnMethods[CSE]
+      {
+
+         //
+
+         type ComparableSpawnedElement = CSE
+
+         /**
+          *
+          * .
+          *
+          */
+         override
+         def spawn()
+         : ComparableSpawnedElement
+
+         /**
+          *
+          * in-place update of it.
+          *
+          */
+         def updateAndSelf(e: ComparableSpawnedElement )
+         : e.type
+
+         /**
+          *
+          * try to generate HTML (it's not always possibly).
+          *
+          * defaults to `spawn().outerHTML`.
+          *
+          */
+         @deprecated
+         def toHTML()
+         : String
+         = spawn().outerHTML
+
+      }
+
+      object PerSampleSpawningAndReconcilingOps
+      {
+         //
+
+         trait CtxtualSpawnMethods[+R] {
+
+            def spawn() : R
+
+         }
+
+      }
+
+      trait ClassReconcilingOps[ComparableSpawnedElement](
+         //
+      )
+      {
+
+         //
+
+         def earlyDeinitInstance(e: ComparableSpawnedElement )
+         : Unit
+         = {}
+
+      }
+
+      @deprecated
+      type Class
+
+      trait ClassOps
+      extends
+      AnyRef
+      with ParentChildRelationship.Cio.ClassOps[ClassOps, ClassInstanceOps]
+      {
+
+         //
+
+         type ComparableSpawnedElement
+            <: ElementBase
+
+         given ttComparableSpawnedElement
+         : reflect.Typeable[ComparableSpawnedElement ]
+
+         val reconciler
+         : AnyRef & ClassReconcilingOps[ComparableSpawnedElement]
+         = new AnyRef with ClassReconcilingOps[ComparableSpawnedElement]
+
+         // type InstanceOps
+         // <: (
+         //    ClassInstanceOps {
+         //       val class1
+         //       : ClassOps.this.type
+         //    }
+         // )
+
+         export reconciler.earlyDeinitInstance
+
+         /* on-topic utility for implementers */
+
+         extension [E](c: PerSampleSpawningAndReconcilingOps[ComparableSpawnedElement] ) {
+
+            /**
+             *
+             * `updateAndSelf` --
+             * in-place update of it.
+             *
+             */
+            // transparent inline
+            def tryUpdateAndSelf(e: ElementBase )
+            : Either[Unit, e.type]
+            = {
+
+               e match {
+
+                  case e1 : ComparableSpawnedElement =>
+                     c updateAndSelf(e1)
+                     Right(e )
+
+                  case _ =>
+                     Left(() )
+
+               }
+            }
+
+         }
+
+      }
+
+      /**
+       * 
+       * this mixin
+       * requires pre-constructed impl for `ttComparableSpawnedElement.` and
+       * narrows `ComparableSpawnedElement#` to `BHA`
+       * 
+       */
+      trait ClassOpsTFixation[
+         BHA <: ElementBase ,
+      ](
+         //
+      )
+      (using override val ttComparableSpawnedElement : reflect.Typeable[BHA] )
+      extends
+      ClassOps
+      {
+
+         //
+
+         //
+
+         override
+         type ComparableSpawnedElement
+            >: BHA <: BHA
+
+      }
+
+   }
+
+   type SjsEOps
+   = SjsEOps.ClassInstanceOps
+
+   ;
+
+   // TODO
+   private[avcframewrk]
+   class SbBcd
+      [
+         BHA
+         <: ElementBase
+         : reflect.Typeable
+         ,
+         Mdl
+         <: scala.Product
+         ,
+      ]
+      ()
+   {
+      //
+
+      object classOpsImpl extends
+      AnyRef
+      with SjsEOps.ClassOps
+      with SjsEOps.ClassOpsTFixation[BHA]
+
+      type ClassInstanceOpsImpl
+      // <: SjsEOps.ClassInstanceOps
+      
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/main.scala
@@ -0,0 +1,348 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e.impl.js
+
+package s1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+final
+lazy val ebAll
+= {
+   new AnyRef
+   with EbAndReconcilability
+   with EbButtons
+}
+
+trait EbButtons
+{
+   this : (
+      AnyRef
+      & EbAndReconcilability
+   ) =>
+   //
+
+   //
+
+   import SjsEOps.{*, given}
+
+   //
+
+   def newClickEvent()
+   : AnyRef & Product
+   = {
+
+      case object clickEvent extends
+         AnyRef
+      clickEvent
+   }
+
+   //
+
+   // HTMLButtonElement | HTMLAElement
+   type BHA
+   =
+      org.scalajs.dom.html.Button
+      | org.scalajs.dom.html.Button
+      | org.scalajs.dom.html.Anchor
+      | org.scalajs.dom.html.Input
+      | org.scalajs.dom.html.Select
+
+   object buttonOrAHrefClassOps
+   extends
+   AnyRef
+   with ClassOps
+   with ClassOpsTFixation[BHA]
+   {
+
+      //
+
+      override
+      type InstanceOps
+      >: ButtonOrAHrefDesc
+      <: ButtonOrAHrefDesc
+
+   }
+
+   case class Edsb[S, +T]
+   (
+      //
+      stateOption: avcframewrk.evm.AsyncAlgebraicMonad[S]
+      ,
+      protected val
+      stateCheck: S => (0 | 0.5 | 1)
+      ,
+      baseTitle: T
+      ,
+      private val
+      stateTitle: (baseTitle: T @annotation.unchecked.uncheckedVariance, state: S ) => T
+      ,
+   )
+   {
+
+      val stateCheckedOption
+      = {
+         stateOption
+         .map[0 | 0.5 | 1 ](stateCheck )
+      }
+
+      val stateTitleOption
+      = {
+         stateOption
+         .map(s => stateTitle(baseTitle, s) )
+      }
+
+   }
+
+   case class ButtonOrAHrefDesc1(
+      edsb: Edsb[?, SjsEOps] ,
+      callback: (evt: AnyRef & Product) => util.Try[Unit] ,
+   )
+   extends
+   AnyRef
+   {
+      //
+
+      val headlineDescr
+      = edsb.baseTitle
+
+   }
+
+   private[EbButtons]
+   def buttonReconciler
+      [
+         ComparableSpawnedElement
+         >: ClassOps#ComparableSpawnedElement & BHA
+         <: ClassOps#ComparableSpawnedElement & BHA
+         : reflect.Typeable
+         ,
+      ]
+      (desc : ButtonOrAHrefDesc1)
+   : PerSampleSpawningAndReconcilingOps[ComparableSpawnedElement]
+   = {
+      //
+
+      import desc.*
+
+      object impl1 extends
+      AnyRef
+      with PerSampleSpawningAndReconcilingOps[ComparableSpawnedElement]
+      {
+         ;
+
+         //
+
+         /** manually clear and (re)populate */
+         private[impl1]
+         def recreateHeading(operand: ComparableSpawnedElement )
+         : headlineDescr.SpawnedElement
+         = {
+            ;
+
+            operand.innerHTML = ""
+
+            val newPeer = headlineDescr.spawn()
+            
+            operand appendChild(newPeer ) 
+
+            newPeer
+         }
+
+         private[impl1]
+         def tryUpdateHeadingOfGivenElemFast(e: ComparableSpawnedElement )
+         : Either[Unit, headlineDescr.ComparableSpawnedElement ]
+         = {
+            //
+
+            e.childNodes.toIndexedSeq match {
+
+               case (ec : headlineDescr.ComparableSpawnedElement) +: Seq() =>
+                  headlineDescr updateAndSelf(ec)
+                  Right(ec )
+
+               case _ =>
+                  Left(() )
+            }
+         }
+
+         private[impl1]
+         def updateHeadingOf(e: ComparableSpawnedElement )
+         : Unit
+         = {
+            //
+
+            /**
+             * 
+             * in general `recreateHeading(e)`, but
+             * strive for `tryUpdateHeadingOfGivenElemFast(e )`
+             * 
+             */
+            tryUpdateHeadingOfGivenElemFast(e)
+            
+            .getOrElse[headlineDescr.ComparableSpawnedElement ]({
+               //
+
+               /** manually clear and (re)populate */
+               val newHeadlineSpawnedE = {
+                  ;
+                  
+                  recreateHeading(e)
+               } : headlineDescr.SpawnedElement
+
+               newHeadlineSpawnedE
+            })
+
+         }
+
+         /**
+          *
+          * in-place update of it.
+          *
+          */
+         override
+         def updateAndSelf(e: ComparableSpawnedElement )
+         : e.type
+         = {
+
+            updateHeadingOf(e)
+
+            /**
+             * 
+             * reassign its `onClick`
+             * 
+             */
+            e.onclick = (
+               (evt) => {
+                  // TODO
+                  callback({ case object clickEvent {} ; clickEvent })
+                  .recover[Unit]({ case scala.util.control.NonFatal(z : Exception) => {
+                     // Thread.sleep(1 * 1000 ) /* not supported in ScJS */
+                     z.printStackTrace()
+                  } })
+                  .get
+               }
+            )
+
+            e
+         }
+
+         //
+
+         /**
+          *
+          * .
+          *
+          */
+         override
+         def spawn()
+         = {
+            // TODO
+            {
+               ;
+
+               val e = {
+                  org.scalajs.dom.document.createElement("button")
+                  .asInstanceOf[ComparableSpawnedElement]
+               }
+
+               /**
+                * 
+                * TODO --
+                * init automatic s
+                * 
+                */
+               edsb.stateCheckedOption
+               .map((vl) => {
+                  e.setAttribute("disabled", vl match { case 0 => "disabled" ; case _ => "" } )
+               } )
+               .countL
+
+               updateAndSelf(e)
+               
+               e
+            }
+         }
+
+         //
+
+         @deprecated
+         override
+         def toHTML()
+         : String
+         = {
+
+            import language.unsafeNulls
+
+            spawn().outerHTML
+            .replaceAll("\\A[<](?:button|a|anchor|input)\\b", "$0 onclick=\"[native code]\" ")
+         }
+
+      }
+      impl1
+   }
+
+   case class ButtonOrAHrefDesc(
+      //
+      desc : ButtonOrAHrefDesc1
+      ,
+   )
+   extends
+   AnyRef
+   with SjsEOps
+   {
+
+      //
+
+      export desc.*
+
+      //
+
+      override
+      val class1
+      : buttonOrAHrefClassOps.type
+      = buttonOrAHrefClassOps
+
+      override
+      type SpawnedElement
+         >: ComparableSpawnedElement
+         <: ComparableSpawnedElement
+
+      val reconciler
+      : PerSampleSpawningAndReconcilingOps[ComparableSpawnedElement]
+      = buttonReconciler[ComparableSpawnedElement](desc = desc )
+
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+val _ = {}
