854e7f9c8842096e666faaa5c8b2f3cf285ca961
me <meh@meh>
2023-08-24 09:11:01 +0700

    [`avcframewrk.forms` for JS] apply these additions

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/laminar.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/laminar.scala
@@ -44,6 +44,98 @@ val laminar
 : laminarReExports.type
 = laminarReExports
 
+implicit
+object LaminarBasedNativeElementRef
+{
+
+   //
+
+   opaque type LlNessAs[+R]
+      = Any
+
+   @deprecated
+   type LlNess
+      = LlNessAs[Any]
+
+   extension [R](e: R) {
+
+      private
+      def asJsDynamic
+      = {
+         ;
+         e.asInstanceOf[scalajs.js.Dynamic]
+      }
+
+   }
+
+   @deprecated
+   def init
+      [R <: org.scalajs.dom.Node ]
+      (e: com.raquo.laminar.nodes.ReactiveNode[R] )
+   : e.ref.type & LlNessAs[R]
+   = {
+      ;
+
+      e.setupBackreference
+
+      e.refWithLlness
+   }
+
+   extension [R <: org.scalajs.dom.Node](e: com.raquo.laminar.nodes.ReactiveNode[R] ) {
+
+      def setupBackreference
+      : e.type
+      = {
+         ;
+
+         e.ref
+         .asJsDynamic.lll_=(e.asInstanceOf[scalajs.js.Any ] )
+
+         e
+      }
+
+      def refWithLlness
+      : e.ref.type & LlNessAs[R]
+      = {
+         ;
+
+         e.ref
+      }
+
+   }
+
+   extension [R <: org.scalajs.dom.Element](e: org.scalajs.dom.Element & LlNessAs[R] ) {
+
+      def devLaminarWrapperE
+      : com.raquo.laminar.nodes.ReactiveElement[e.type]
+      = {
+         ;
+
+         e
+         .asJsDynamic.lll
+         .asInstanceOf[com.raquo.laminar.nodes.ReactiveElement[e.type] ]
+         .nn
+      }
+
+   }
+
+   extension [R <: org.scalajs.dom.html.Element](e: org.scalajs.dom.html.Element & LlNessAs[R] ) {
+
+      def devLaminarWrapperEH
+      : com.raquo.laminar.nodes.ReactiveHtmlElement[e.type]
+      = {
+         ;
+
+         e
+         .asJsDynamic.lll
+         .asInstanceOf[com.raquo.laminar.nodes.ReactiveHtmlElement[e.type] ]
+         .nn
+      }
+
+   }
+
+}
+
 val Airstream
 : laminarReExports.api.A.type
 = laminarReExports.api.A
---
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/main.scala
@@ -0,0 +1,122 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e
+
+
+
+
+
+
+
+
+
+
+
+
+
+// protected
+// final
+// lazy
+// val allDefsSjsImpl
+// = {
+
+//    object jsWindow {
+
+//       // export org.scalajs.dom.{
+//       //    Element ,
+//       // }
+
+//    }
+
+//    import ebAll.{*, given}
+
+//    {
+//       //
+
+//       new AnyRef
+//       with avcframewrk.forms.templating.w3e.pre.Articles
+//       with avcframewrk.forms.templating.w3e.pre.Buttons
+//       {
+
+//          override
+//          type Article
+//          >: SjsEOps
+//          <: SjsEOps
+
+//          override
+//          val Action
+//          = {
+//             AcsfLabelledCallbackTranslator(
+//                //
+//                doFinalAdapt = (_, run, s) => {
+//                   // TODO
+//                   val edsb = {
+//                      Edsb[s._1.State, s._1.Title](
+//                         //
+
+//                         stateOption = s._2._1 ,
+//                         stateCheck = s._2._2 ,
+//                         baseTitle = s._2._3 ,
+
+//                         // TODO
+//                         stateTitle = ??? ,
+                        
+//                      )
+//                   }
+//                   (edsb, () => { run().left.map({ case z: Throwable => z ; case () => new RuntimeException }).toTry.get } )
+//                } ,
+//             )
+//          }
+
+//          override
+//          type Action
+//             >: (Edsb[?, ?], () => Unit)
+//             <: (Edsb[?, ?], () => Unit)
+
+//          ;
+//          type ButtonArt
+//             >: ButtonOrAHrefDesc
+//             <: ButtonOrAHrefDesc
+
+//          override
+//          def describeButtonByAction(action: Action)
+//          = {
+//             ButtonOrAHrefDesc(
+//                //
+//                desc = {
+//                   ButtonOrAHrefDesc1(
+//                      //
+//                      edsb =
+//                         // Edsb(stateOption = ???)
+//                         // action._1
+//                         ???
+//                      ,
+//                      callback = (ev) => util.Try({ action._2.apply() })
+//                      ,
+//                   )
+//                },
+//             )
+//          }
+
+//       }
+//    }
+// } // allDefsImpl$
+
+// export allDefsSjsImpl.{*, given}
+
+export impl.js.s2.allDefsSjsImpl.{*, given}
+
+
+
+
+
+
+
+
+
+
+
+
+val _ = {}
