3a22ad140392f8cc25fccc6081628f095c66b493
me <meh@meh>
2023-08-27 21:34:15 +0700

    apply these changes

---
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/eb.scala
@@ -0,0 +1,51 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e.impl.js
+
+package s1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+trait EbAndReconcilability
+extends
+AnyRef
+{
+   //
+
+   ;
+
+   export avcframewrk.forms.templating.rendering.reconciliabilityC.*
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
