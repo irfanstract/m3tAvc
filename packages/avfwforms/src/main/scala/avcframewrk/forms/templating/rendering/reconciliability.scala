f31fbbb1cfcda2d7c78c02c06f2a8170af2e373d
me <meh@meh>
2023-08-27 16:47:11 +0700

    [`avcframewrk.forms`] [Templating] [Renderers] apply these additions (`reconciliability.scala`)

---
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/rendering/reconciliability.scala
@@ -0,0 +1,114 @@
+package avcframewrk.forms
+
+package templating
+
+package rendering
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ * in case
+ * the creation of a new peer would be unacceptably inefficient/expensive,
+ * it'd be better to (simply) reuse the existing peer at the place
+ * 
+ */
+object reconciliabilityC
+{
+   ;
+
+   trait SpawnabilityNoArg[-Mdl, +R ]
+   {
+      ;
+
+      extension (this1: Mdl)
+         def spawn()
+         : R
+
+   }
+
+   trait SpawnabilityA1[-Mdl, -A, +R ]
+   {
+      ;
+
+      extension (this1: Mdl)
+         def spawn(arg: A)
+         : R
+
+   }
+
+   /**
+    * in case
+    * the creation of a new peer would be unacceptably inefficient/expensive,
+    * it'd be better to (simply) reuse the existing peer at the place
+    * 
+    */
+   trait Reconciliability[-Receiver, -Mdl, +R ]
+   {
+      ;
+
+      extension (this1: Receiver)
+         def model_=(newModel: Mdl )
+         : R
+
+   }
+
+   /**
+    * simultaneous `SpawnabilityNoArg` and `Reconciliability`
+    * 
+    * the type `R` is only used for the "reconcile" methods -
+    * `spawn`'s return-type has already been narrowed to `Spawned` so we can't make it `R`.
+    * additionally,
+    * it's possible to
+    * further narrow `spawn`'s return-type by re-`extends`ing `SpawnabilityNoArg` with more-specific `R`
+    * 
+    */
+   trait SpawnabilityAndReconciliabilityNoArg[-Mdl, Spawned, +R]
+   extends
+   AnyRef
+   with SpawnabilityNoArg[Mdl, Spawned]
+   with Reconciliability[Spawned, Mdl, R]
+   {
+      this : (
+         AnyRef
+      ) =>
+
+      ;
+
+      ;
+   }
+
+   ;
+} // reconciliabilityC$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+val _ = {}
