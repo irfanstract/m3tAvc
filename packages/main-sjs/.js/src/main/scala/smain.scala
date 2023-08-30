3b286e5eaf3ae9c88d730d378a6f2de5d4fc206b
me <meh@meh>
2023-08-30 22:42:04 +0700

    apply these changes

--- packages/main-sjs/.js/src/main/scala/smain.scala
+++ packages/main-sjs/.js/src/main/scala/smain.scala
@@ -18,6 +18,17 @@ def runSMain(): Unit
    ()
    ()
    println(s"hello!")
+   ()
+   locally {
+      import org.scalajs.dom
+      import com.raquo.laminar.api.L
+      L.renderOnDomContentLoaded(dom.document.querySelector("#app") match { case e => e.innerHTML = "" ; e } , {
+         val a
+         = avcframewrk.forms.templating.w3e.impl.js.s1.ebAll.PlainLocaleStringPlainTxtArticle({ import java.util.Locale ; (Locale.ROOT.nn , "hello" ) } )
+         // identity { import L.* ; img(src := "/vite.svg", className := "logo", alt := "Vite logo") }
+         a.spawn()
+      } )
+   }
 }
 
 
