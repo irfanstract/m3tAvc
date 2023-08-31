f67d7581ecc00ef6453cbf1891f98fcd8469c42d
me <meh@meh>
2023-08-31 10:18:38 +0700

    [`avcframewrk.forms`] [Templating, W3E] init `plain-txts.scala` and then impl `PlainTxtContents` this way

---
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/w3e/pre/plain-txts.scala
@@ -0,0 +1,62 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e.pre
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+trait PlainTxtContents
+extends
+   AnyRef
+   /* with these items item merely listed in the self-type, the IDE/editor won't show any relevant "overrides super member" markers */
+   with w3e.pre.Articles
+   /* a temporary treat necessary to prevent the compiler from hanging */
+   // with ELaminarQckCoreHtml
+{
+   this : (
+      AnyRef
+      with w3e.pre.Articles
+   ) =>
+   ;
+
+   ;
+
+   val PlainLocaleStringPlainTxtArticle
+   : (
+      AnyRef
+      & (
+         (locale: java.util.Locale, txt: String) =>
+            Article
+      )
+   )
+
+   ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+val _ = {}
