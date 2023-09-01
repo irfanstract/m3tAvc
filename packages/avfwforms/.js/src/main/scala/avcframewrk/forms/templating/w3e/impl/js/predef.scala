a79aa252bfa292c35ad109db5f4bd5d658ba7c51
me <meh@meh>
2023-09-01 23:04:44 +0700

    [`avcframewrk.forms`] [Templating] [`w3e.impl.js.s1`] impl `trySameThreadScheduler` and `newValueUpdateRepipe` and `newCallbackImplUpdateRepipe`

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/predef.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/predef.scala
@@ -130,6 +130,98 @@ def closeAllOf
    ;
 } // closeAllOf
 
+given [T0]
+: util.Using.Releasable[monix.reactive.Observer[?] ]
+= r => r.onComplete()
+
+/**
+ * strives for `monix.execution.Scheduler(concurrent.ExecutionContext.parasitic )`, but
+ * since the factory overload was unavailable/missing in the JS build of Monix
+ * we needed to resort back to `monix.execution.Scheduler.global`
+ * 
+ */
+def trySameThreadScheduler()
+: monix.execution.Scheduler
+= monix.execution.Scheduler(concurrent.ExecutionContext.parasitic )
+
+/**
+ * like "pipe"s, but
+ * instead of individual items we pass `Observable`s instead
+ * 
+ */
+def newValueUpdateRepipe[R](
+   //
+   prototype
+      : (value: R) => Any
+   ,
+   initialValue
+      : R
+   ,
+)
+= {
+   ;
+
+   implicit val scheduler
+   = trySameThreadScheduler()
+
+   laminar.api.L.Var[AsyncStateChangeMonad[R] ](laminar.api.L.Val(initialValue ) )
+   match { case pipe => {
+      (pipe.writer, {
+         pipe.signal
+         .flatten
+      } )
+   } }
+} // newValueUpdateRepipe
+
+/**
+ * the consumer-side itc ref will never change identity ; only the impl will change .
+ * like "pipe"s, but
+ * instead of individual fnc(s) we pass `Observable`s emitting fnc(s) instead .
+ * 
+ */
+def newCallbackImplUpdateRepipe
+   [
+      A,
+      R,
+   ]
+   (
+      //
+      prototype
+         : (arg: A) => R
+      ,
+      initialImpl
+         : A => R
+      = (_: Any) => { throw new IllegalStateException(s"no initial impl") }
+      ,
+   )
+= {
+   ;
+
+   type F
+   = (argOrCtx: A) => R
+
+   implicit val scheduler
+   = trySameThreadScheduler()
+
+   newValueUpdateRepipe(
+      //
+      prototype = (a: A => R ) => {} ,
+      initialValue = initialImpl ,
+   )
+   match { case (_1, _2) => {
+      ;
+
+      var vle
+      : F
+      = initialImpl
+
+      _2
+      .map(c => { vle = c } )
+
+      locally[(_1.type, F )](_1, { (arg: A) => vle.apply(arg) } )
+   } }
+} // newCallbackImplUpdatePipe
+
 
 
 
