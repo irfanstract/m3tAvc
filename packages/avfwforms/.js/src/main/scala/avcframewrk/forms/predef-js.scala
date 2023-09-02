10bb08a6148a1efdbed004cc58b6cac9df6ac7d8
me <meh@meh>
2023-09-02 16:59:43 +0700

    [`avcframewrk.forms`] [JS/DOM] impl `CSS` and `addGlobalCss`

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/predef-js.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/predef-js.scala
@@ -16,6 +16,34 @@ package avcframewrk.forms
 
 
 
+opaque type CSS
+= String
+
+extension (v: CSS )
+   def toCSSString()
+   : String
+   = v
+
+@deprecated("this Conversion does not run any validation.")
+given Conversion[String, CSS ]
+= identity[String] _
+
+def addGlobalCss
+  (addend: CSS )
+: Unit
+= {
+
+   import org.scalajs.dom
+
+   val c = dom.document.createElement("style")
+   dom.document.head
+   .appendChild(c )
+
+   c.textContent = addend.toCSSString()
+
+   ;
+}
+
 
 
 
