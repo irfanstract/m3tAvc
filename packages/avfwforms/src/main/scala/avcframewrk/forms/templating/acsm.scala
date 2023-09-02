13bf6bf35f322ae24e4aad443a368d81ef08da92
me <meh@meh>
2023-09-02 09:18:30 +0700

    [`avcframewrk.forms`] [Templating] [Renderers] re-impl `AsyncStateChangeMonad#` and `EventMonad#`
    now `AsyncStateChangeMonad#` is opaque.
    the `extension`
    re-export-s all methods from `com.raquo.airstream.core.Signal` with rename(s) (`scanLeft` as `scanLeftAdapted0`) and
    adds `toLaminarObservable`

--- packages/avfwforms/src/main/scala/avcframewrk/forms/templating/acsm.scala
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/acsm.scala
@@ -17,14 +17,72 @@ package templating
 
 
 
-type AsyncStateChangeMonad[+A]
-   >: com.raquo.airstream.core.Signal[A]
-   <: com.raquo.airstream.core.Signal[A]
+opaque type AsyncStateChangeMonad[+A]
+   <: (
+      AnyRef
+      // & com.raquo.airstream.core.Signal[A]
+   )
+   =  com.raquo.airstream.core.Signal[A]
    /* what about `StrictSignal` ? */
 
-type EventMonad[+A]
-   >: com.raquo.airstream.core.EventStream[A]
-   <: com.raquo.airstream.core.EventStream[A]
+object AsyncStateChangeMonad
+{
+   ;
+
+   given [A]
+   : Conversion[com.raquo.airstream.core.Signal[A] , AsyncStateChangeMonad[A] ]
+   = identity[com.raquo.airstream.core.Signal[A] ] _
+
+   extension [A] (this10: AsyncStateChangeMonad[A] ) {
+      def this11
+      /* widen the type, to build on the opaque alias */
+      : com.raquo.airstream.core.Signal[A] & com.raquo.airstream.core.BaseObservable[AsyncStateChangeMonad, A]
+      = this10
+
+      export this11.{
+         scanLeft as _,
+         *, given ,
+      }
+
+      /* `scanLeft` was excluded above ; exposing it here renamed */
+      export this11.{scanLeft as scanLeftAdapted0 }
+
+      def toLaminarObservable
+      : com.raquo.airstream.core.Signal[A]
+      = this11
+
+   }
+
+   ;
+} // AsyncStateChangeMonad$
+
+opaque type EventMonad[+A]
+   <: (
+      AnyRef
+      & com.raquo.airstream.core.EventStream[A]
+   )
+   =  com.raquo.airstream.core.EventStream[A]
+
+object EventMonad
+{
+   ;
+
+   given [A]
+   : Conversion[com.raquo.airstream.core.EventStream[A] , EventMonad[A] ]
+   = identity[com.raquo.airstream.core.EventStream[A] ] _
+
+   extension [A] (this10: EventMonad[A] ) {
+      def this11
+      /* widen the type, to build on the opaque alias */
+      : com.raquo.airstream.core.BaseObservable[EventMonad, A]
+      = this10
+
+      export this11.{*, given }
+
+   }
+
+   ;
+} // EventMonad$
 
 type AsyncEventMonad[+A]
    = EventMonad[A]
