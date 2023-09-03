8eb898f5d82ed4ef4bd22d5106c733a6f8c1fe25
me <meh@meh>
2023-09-03 08:42:39 +0700

    [`avcframewrk.forms`] [Templating, W3E] [`buttons.scala`] apply these changes (mostly about enhancing `VarEditingActionsProv` )

--- packages/avfwforms/src/main/scala/avcframewrk/forms/templating/w3e/pre/buttons.scala
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/w3e/pre/buttons.scala
@@ -80,10 +80,12 @@ object VarEditingActionsProv
 trait VarEditingActionsProv
 extends
 AnyRef
+with StdGsps
 {
    this : (
       AnyRef
       & Buttons
+      & StdGsps
    ) =>
    ;
 
@@ -101,12 +103,189 @@ AnyRef
    : (
       AnyRef
 
+      & (
+         /** 
+          * `[Value given GivenSpinner] -> (varLike : VarLike[Value] ) -> Action`
+          * 
+          * anticipating some 3rd party libs which provides "mapped var(s)" which `extends` both these `trait`s yet does not `extends` `Var#`.
+          * `Var[type A]#` `extends` both `SignalSource[A]#` and `Sink[A]#`
+          * 
+          */
+         [Value] =>
+         (baseTitle: String | Article ) =>
+         (operand: L.SignalSource[Value] & L.Sink[Value] ) =>
+         (GivenSpinner[Value] ) ?=>
+            Action
+      )
+
    )
+   lazy val _ = {
+      val f = VarEditingAction
+      f.apply("")(??? : L.Var["5"] )
+   }
 
    ;
 
    ;
 } // VarEditingActionsProv#
+
+/**
+ * 
+ * defs specifically for properly implementing `VarEditingAction` in `VarEditingActionsProv`
+ * 
+ * every instance of `GivenSpinner[type Value]#`
+ * shall serve the minimums to spin/edit values each an instance of `Value#`
+ * 
+ */
+trait StdGsps
+extends
+AnyRef
+{
+   this : (
+      AnyRef
+   ) =>
+   ;
+
+   ;
+
+   /**
+    * support for editor for `Value#`
+    * 
+    * `Value#` is contra-variant
+    * 
+    */
+   type GivenSpinner[-Value]
+
+   given given_GivenSpinner_String
+   : GivenSpinner[String]
+
+   type GivenSpinnerNumerical[-Value <: AnyVal | java.lang.Number ]
+   = GivenSpinner[Value]
+
+   given given_GivenSpinner_Number
+      [Value <: (
+         java.lang.Number
+         | BigDecimal
+         | (Double | Float )
+         | (java.math.BigInteger | BigInt | Long | Int | Short | Byte )
+      )]
+      (using Numeric[Value ] )
+   : GivenSpinnerNumerical[Value ]
+
+   given given_GivenSpinner_Boolean
+   : GivenSpinner[Boolean]
+
+   ;
+} // StdGsps#
+
+/**
+ * 
+ * `StdGsps`
+ * 
+ * there's an impl, `ofSnb` ;
+ * simply `export StdGsps.ofSnb.{*, given }` in a sub-class
+ * 
+ */
+object StdGsps
+{
+   ;
+   
+   ;
+
+   /**
+    * 
+    * an impl of `StdGsps` based on `Numeric#parseString` and `PartialFunction`
+    * 
+    */
+   final
+   lazy val ofSnb
+   : ofSnbImpl.type
+   = {
+      ofSnbImpl
+   }
+
+   private[StdGsps]
+   given ofSnbImpl
+   : StdGsps
+   with {
+      ;
+
+      ;
+
+      type GivenSpinner[-Value]
+      >: GivenSpinner1[? >: Value]
+      <: GivenSpinner1[? >: Value]
+
+      trait GivenSpinner1
+         [Value]
+         (val parse: PartialFunction[String, Value] )
+      extends
+      AnyRef
+      with PartialFunction[String, Value]
+      {
+
+         export parse.isDefinedAt
+         export parse.apply
+
+         ;
+      }
+      object GivenSpinner1
+      {
+         ;
+
+         ;
+      }
+
+      quoted.Type
+
+      ;
+
+      given given_GivenSpinner_String
+      : AnyRef
+         with GivenSpinner1[String](parse = { case v => v } )
+
+      ;
+
+      // override
+      given given_GivenSpinner_Number
+         [Value <: (
+            java.lang.Number
+            | BigDecimal
+            | (Double | Float )
+            | (java.math.BigInteger | BigInt | Long | Int | Short | Byte )
+         )]
+         (using Numeric[Value ] )
+      : AnyRef
+         with GivenSpinner1(
+            parse = (
+               //
+
+               // TODO
+               (summon[Numeric[Value ] ].parseString _ )
+               match { case f => f.unlift }
+               match { case f => new AnyRef with GivenSpinner1(parse = f ) }
+
+            ) ,
+         )
+
+      ;
+
+      given given_GivenSpinner_Boolean
+      : AnyRef
+      with GivenSpinner1[Boolean](
+         parse = {
+            case "tru"  |  "true" | "True"  |  "TRUE" | "1" =>  true
+            case "fals" | "false" | "False" | "FALSE" | "0" => false
+         } ,
+      )
+
+      ;
+   }
+
+   ;
+
+   ;
+} // StdGsps.
 
 
 
