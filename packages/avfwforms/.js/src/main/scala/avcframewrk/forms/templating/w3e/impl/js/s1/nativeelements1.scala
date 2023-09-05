a97a9f5140143d26d718ea7907a6b316ec213821
me <meh@meh>
2023-09-05 10:02:03 +0700

    [`avcframewrk.forms`] [Templating, W3E] [`impl.js.s1.`] impl `given_SpawnabilityAndReconciliability_CaseClassGeneralised1`

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/nativeelements1.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/nativeelements1.scala
@@ -133,6 +133,194 @@ extends
    ;
 } // ENativeElementsD1
 
+;
+
+/**
+ * 
+ * generalisation of `SpawnabilityAndReconciliabilityNoArg` to arbitrary 'element's
+ * 
+ */
+object given_SpawnabilityAndReconciliability_CaseClassGeneralised1
+extends
+AnyRef
+with ELaminarQckCoreABackreferencings
+{
+   ;
+
+   ;
+
+   import com.raquo.laminar.{nodes as ln}
+
+   import org.scalajs.dom
+
+   ;
+
+   ;
+
+   def apply
+      [
+         XModel <: Matchable & Product ,
+         NativeE <: dom.HTMLElement ,
+      ]
+      (srcToSetterPairs: (SrcLensAndDestAttrPair[XModel, ? ] )* )
+      (
+         nativeElemLCtor : com.raquo.laminar.tags.HtmlTag[NativeE]
+         ,
+      )
+   : SpawnabilityAndReconciliabilityNoArg[XModel, ?, Unit ]
+   = {
+      ;
+
+      class XEAndStateBag1() extends
+      aBackreferencings.XEAndStateBag(ec = { laminar.api.L.a })
+      with aBackreferencings.XEAndStateBagCm[XModel, Unit]
+      {
+         //
+
+         import laminar.api.L
+
+         override
+         def close(): Unit
+         = {
+            closeAllOf[laminar.api.L.Observer[?] ]((
+               srcToSetterDispatchers
+
+            ))
+         }
+
+         val srcToSetterDispatchers
+         = {
+            for ((f : SrcLensAndDestAttrPair[md, v] ) <- srcToSetterPairs )
+            yield {
+               ;
+               val aPiper
+               = (f.dest ).startAttribNow(initialValue = f.initialValue )
+               ;
+               ({
+                  ;
+
+                  aPiper
+                  .contramap((src: md ) => L.Val(f.distillMdl(src) ) )
+               } )
+            }
+         } : Seq[L.Observer[XModel ] ]
+
+         override
+         def model_=(newMdl: XModel): Unit
+         = {
+            for (o <- srcToSetterDispatchers )
+            do { o.onNext(newMdl ) }
+         }
+
+         // def
+
+         ;
+
+      }
+
+      ({
+         import aBackreferencings.{given Conversion[?, ?] }
+
+         summon[Conversion[() => XEAndStateBag1 , ? ] ]
+         .apply(() => new XEAndStateBag1() )
+      })
+   }
+
+   case class SrcLensAndDestAttrPair[XModel, V](impl : (
+      (
+         (mdl: XModel) => V
+         ,
+         V
+         ,
+         (com.raquo.laminar.keys.HtmlAttr[V] | com.raquo.laminar.keys.HtmlProp[V, V] )
+         ,
+      )
+   ) )
+   {
+      ;
+
+      export impl.{_3 as dest }
+
+      export impl.{_2 as initialValue }
+      export impl._1.{apply as distillMdl }
+
+      private[w3e]
+      type ImplG
+      = impl.type
+
+      ;
+   }
+
+   /**
+    * automatic conv from `Tuple3` to `SrcLensAndDestAttrPair[XModel, V]`
+    * 
+    */
+   given asSrcLensAndDestAttrPair[XModel, V]
+   : (
+      Conversion[
+         //
+         (
+            (mdl: XModel) => V
+            ,
+            V
+            ,
+            (com.raquo.laminar.keys.HtmlAttr[V] | com.raquo.laminar.keys.HtmlProp[V, V] )
+            ,
+         )
+         ,
+         SrcLensAndDestAttrPair[XModel, ? ]
+         ,
+      ]
+   )
+   = {
+      c => SrcLensAndDestAttrPair(c)
+   }
+
+   identity(() )
+   identity(() )
+
+   ;
+
+   ;
+} // given_SpawnabilityAndReconciliability_CaseClassGeneralised1.
+
+;
+
+/**
+ * actually test the type-ing
+ * 
+ */
+val _ = {
+   ;
+
+   locally {
+      ;
+
+      given_SpawnabilityAndReconciliability_CaseClassGeneralised1(
+         //
+         ((_: Tuple3[?, String, ?] )._2, "(unknown)", laminar.api.L.idAttr )
+         ,
+         ((_: Tuple3[Boolean, String, BigInt] )._1, false, laminar.api.L.checked )
+         ,
+
+      )(nativeElemLCtor = laminar.api.L.div )
+   }
+
+   locally {
+      ;
+
+      given_SpawnabilityAndReconciliability_CaseClassGeneralised1(
+         //
+         ((_: Tuple3[?, java.net.URI, ?] )._2.toASCIIString().nn , "(unknown)", laminar.api.L.href )
+         ,
+         ((_: Tuple3[Boolean, ?, BigInt] )._1, false, laminar.api.L.hidden )
+         ,
+
+      )(nativeElemLCtor = laminar.api.L.a )
+   }
+
+} // "actually test the type-ing"
+
 
 
 
