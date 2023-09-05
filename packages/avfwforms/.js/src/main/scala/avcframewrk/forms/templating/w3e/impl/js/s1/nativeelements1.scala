208d2dd709c3c7c145577a2b0ce92e395348d287
me <meh@meh>
2023-09-05 10:02:03 +0700

    [`avcframewrk.forms`] [Templating, W3E] [`impl.js.s1.`] impl `given_SpawnabilityAndReconciliability_CaseClassGeneralised1`

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/nativeelements1.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/nativeelements1.scala
@@ -133,6 +133,238 @@ extends
    ;
 } // ENativeElementsD1
 
+;
+
+/**
+ * 
+ * generalisation of `SpawnabilityAndReconciliabilityNoArg` to arbitrary 'element's
+ * 
+ */
+object given_SpawnabilityAndReconciliability_CaseClassGeneralised1
+extends
+AnyRef
+with ELaminarQckCoreABackreferencings
+{
+   ;
+
+   ;
+
+   import com.raquo.laminar.{nodes as ln}
+
+   import org.scalajs.dom
+
+   ;
+
+   ;
+
+   def apply
+      [
+         XModel <: Matchable & Product ,
+         NativeE <: dom.HTMLElement ,
+      ]
+      (srcToSetterPairs: SrcLensAndDestAttrPairRawFunc[XModel] * )
+      (
+         nativeElemLCtor : com.raquo.laminar.tags.HtmlTag[NativeE]
+         ,
+      )
+   : SpawnabilityAndReconciliabilityNoArg[XModel, ln.ReactiveHtmlElement[NativeE], Unit ]
+   = {
+      ;
+
+      class XEAndStateBag1() extends
+      aBackreferencings.XEAndStateBag(ec = { nativeElemLCtor })
+      with aBackreferencings.XEAndStateBagCm[XModel, Unit]
+      {
+         //
+
+         import laminar.api.L
+
+         override
+         def close(): Unit
+         = {
+            closeAllOf[laminar.api.L.Observer[?] ]((
+               srcToSetterDispatchers
+
+            ))
+         }
+
+         val srcToSetterDispatchers
+         = {
+            for ((f ) <- srcToSetterPairs )
+            yield {
+               ;
+               ({
+                  ;
+
+                  (f.apply((
+                     //
+
+                     [V] =>
+                     (distill: (mdl: XModel) => V, defaultValue: V ) =>
+                     (targetedAttr: com.raquo.laminar.keys.HtmlAttr[V] | com.raquo.laminar.keys.HtmlProp[V, V] ) =>
+                     {
+                        ;
+                        ((distill, defaultValue, targetedAttr ) )
+                        match { case p => p : SrcLensAndDestAttrPairRaw[XModel, V ] }
+                        match { case p => SrcLensAndDestAttrPair(p) }
+                     }
+                  ) ) )
+                  match { case f1 => (f1 : SrcLensAndDestAttrPair[XModel, ? ] ) }
+               })
+               match { case f: SrcLensAndDestAttrPair[md, v ] => {
+               ;
+               
+               ;
+               val aPiper
+               = (f.dest ).startAttribNow(initialValue = f.initialValue )
+               ;
+               ({
+                  ;
+
+                  aPiper
+                  .contramap((src: md ) => L.Val(f.distillMdl(src) ) )
+               } )
+               } }
+            }
+         } : Seq[L.Observer[XModel ] ]
+
+         override
+         def model_=(newMdl: XModel): Unit
+         = {
+            for (o <- srcToSetterDispatchers )
+            do { o.onNext(newMdl ) }
+         }
+
+         // def
+
+         ;
+
+      }
+
+      ({
+         import aBackreferencings.{given Conversion[?, ?] }
+
+         val f1
+         = () => new XEAndStateBag1().wrappedLaminarElement
+
+         summon[Conversion[f1.type, SpawnabilityAndReconciliabilityNoArg[XModel, ? <: ln.ReactiveHtmlElement[dom.HTMLElement] , ? ] ] ]
+         .apply(f1 )
+      })
+   }
+
+   type SrcLensAndDestAttrPairRawFunc[-XModel]
+   = SrcLensAndDestAttrPairRawFuncInvar[XModel @annotation.unchecked.uncheckedVariance ]
+   type SrcLensAndDestAttrPairRawFuncInvar[XModel]
+   = (
+      (f: (
+         [V] =>
+         (distill: (mdl: XModel) => V, defaultValue: V ) =>
+         (targetedAttr: com.raquo.laminar.keys.HtmlAttr[V] | com.raquo.laminar.keys.HtmlProp[V, V] ) =>
+            SrcLensAndDestAttrPair[XModel, V ] 
+      ) ) =>
+         SrcLensAndDestAttrPair[XModel, ? ] 
+   )
+
+   type SrcLensAndDestAttrPairRaw[-XModel, V] =  (
+      (
+         (mdl: XModel) => V
+         ,
+         V
+         ,
+         (com.raquo.laminar.keys.HtmlAttr[V] | com.raquo.laminar.keys.HtmlProp[V, V] )
+         ,
+      )
+   )
+
+   case class SrcLensAndDestAttrPair[-XModel, V](impl : (
+      SrcLensAndDestAttrPairRaw[XModel, V ]
+   ) )
+   {
+      ;
+
+      export impl.{_3 as dest }
+
+      export impl.{_2 as initialValue }
+      export impl._1.{apply as distillMdl }
+
+      private[w3e]
+      type ImplG
+      = impl.type
+
+      ;
+   }
+
+   /**
+    * automatic conv from `Tuple3` to `SrcLensAndDestAttrPair[XModel, V]`
+    * 
+    */
+   given asSrcLensAndDestAttrPair[XModel, V]
+   : (
+      Conversion[
+         //
+         SrcLensAndDestAttrPairRaw[XModel, V ]
+         ,
+         SrcLensAndDestAttrPair[XModel, ? ]
+         ,
+      ]
+   )
+   = {
+      c => SrcLensAndDestAttrPair(c)
+   }
+
+   identity(() )
+   identity(() )
+
+   ;
+
+   ;
+} // given_SpawnabilityAndReconciliability_CaseClassGeneralised1.
+
+;
+
+/**
+ * actually test the type-ing
+ * 
+ */
+val _ = {
+   ;
+
+   import org.scalajs.dom
+
+   locally {
+      ;
+
+      given_SpawnabilityAndReconciliability_CaseClassGeneralised1[
+         Tuple3[Boolean, String, BigInt] ,
+         dom.HTMLDivElement ,
+      ] (
+         //
+         cf => cf((_ )._2, "(unknown)" )(laminar.api.L.idAttr )
+         ,
+         cf => cf((_ )._1, false )(laminar.api.L.checked )
+         ,
+
+      )(nativeElemLCtor = laminar.api.L.div )
+   }
+
+   locally {
+      ;
+
+      given_SpawnabilityAndReconciliability_CaseClassGeneralised1[
+         Tuple3[Boolean, java.net.URI, BigInt] ,
+         dom.HTMLAnchorElement ,
+      ] (
+         //
+         cf => cf((_ )._2.toASCIIString().nn , "(unknown)" )(laminar.api.L.href  )
+         ,
+         cf => cf((_ )._1, false )(laminar.api.L.hidden )
+         ,
+
+      )(nativeElemLCtor = laminar.api.L.a )
+   }
+
+} // "actually test the type-ing"
+
 
 
 
