359eb612bada1517846f60ec6fd214ec5835455a
me <meh@meh>
2023-09-08 06:16:01 +0700

    [`avcframewrk.forms`] [Templating, W3E] [`impl.js.s1`] apply these changes (moving these `SpiwmTwos`-related stuffs up (in)to the `any-backend` work-tree, leaving the `exports` of them in another new src-file )

---
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/reconciliation1.scala
@@ -0,0 +1,62 @@
+package avcframewrk.forms
+
+package templating
+
+package w3e.impl.js
+
+package s1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * a pair, which's
+ * a `SpawnabilityAndReconciliabilityNoArg` and a "data-model"
+ * 
+ * refines the tuple-type to achieve that 'dependent-typing' of `_1`
+ * 
+ */
+export templating.rendering.SpiwmTwos
+
+export templating.rendering.{spawnThisSpiwmTwos, doSpiwmTwoReconciliationOf }
+
+val _ = {
+   ((arg : SpiwmTwos[1, 2, 3] ) => {})
+   .nn
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+val _ = {}
