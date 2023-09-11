2e41eafdfff718d4e58ddbe5130aa8672ed00a30
me <meh@meh>
2023-09-11 11:35:26 +0700

    [`avcframewrk.forms`] [Templating] apply these changes to `AsyncStateChangeMonad` and `EventMonad`
    the changes include
    getting rid of 'wildcard-export's and
    an F-bounding fix to `EventMonad`

--- packages/avfwforms/src/main/scala/avcframewrk/forms/templating/acsm.scala
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/acsm.scala
@@ -47,13 +47,115 @@ object AsyncStateChangeMonad
       : com.raquo.airstream.core.Signal[A]
       = this10
 
+      /*  */
       export thisAscm.{
-         scanLeft as _,
-         *, given ,
-      }
+         //
 
-      /* `scanLeft` was excluded above ; exposing it here renamed */
-      export thisS.{scanLeft as scanLeftAdapted0 }
+         map ,
+         mapTo ,
+         mapToStrict ,
+         mapToUnit ,
+         matchStreamOrSignal ,
+
+         flatMap ,
+         // foldLeft ,
+         // foldLeftRecover ,
+         foreach ,
+
+         //
+      }
+      export thisAscm.{given}
+      export thisS.{
+         //
+
+         changes ,
+         // combineSeq ,
+         compose ,
+         composeAll ,
+         composeChanges ,
+         // currentValueFromParent ,
+         debugWith ,
+         // defaultDisplayName ,
+         displayName ,
+         setDisplayName ,
+         distinct ,
+         distinctBy ,
+         distinctByFn ,
+         distinctByRef ,
+         distinctErrors ,
+         distinctTry ,
+         // removeExternalObserver ,
+         // addExternalObserver ,
+         // removeExternalObserverNow ,
+         foldLeft ,
+         foldLeftRecover ,
+         // flatMap , /* refined above */
+         // foldLeft ,
+         // foldLeftRecover ,
+         // foreach ,
+         // fromCustomSource ,
+         // fromFuture ,
+         // fromJsPromise ,
+         // fromTry ,
+         // fromValue ,
+         // addInternalObserver ,
+         // removeInternalObserver ,
+         // removeInternalObserverNow ,
+         // isSafeToRemoveObserver ,
+         // isSafeToRemoveObserver_= ,
+         // isStarted ,
+         // lastUpdateId ,
+         // map , /* refined above */
+         // mapTo ,
+         // mapToStrict ,
+         // mapToUnit ,
+         // matchStreamOrSignal ,
+         // maybeDisplayName ,
+         // // maybeDisplayName_= ,
+         // maybePendingObserverRemovals ,
+         // // maybePendingObserverRemovals_= ,
+         // maybeWillStart ,
+         // nextUpdateId ,
+         // now ,
+         // numAllObservers ,
+         observe ,
+         addObserver ,
+         // onAddedExternalObserver ,
+         // onStart ,
+         // onStop ,
+         // onWillStart ,
+         // getOrCreatePendingObserverRemovals ,
+         // protectedAccessEvidence ,
+         recover ,
+         recoverIgnoreErrors ,
+         recoverToTry ,
+         scanLeft as scanLeftAdapted0 , /* redefined below */
+         scanLeftRecover as scanLeftRecover0 ,
+         // sequence ,
+         throwFailure ,
+         // topoRank ,
+         // tryNow ,
+         // toCombinableSignal ,
+         // toDebuggableSignal ,
+         toObservable ,
+         // toSignalCompanionCombineSyntax ,
+         toSignalIfStream ,
+         // toSplittableOneSignal ,
+         // toSplittableOptionSignal ,
+         // toSplittableSignal ,
+         toStreamIfSignal ,
+         // toTupleSignal2 ,
+         // toTupleSignal3 ,
+         // toTupleSignal4 ,
+         // toTupleSignal5 ,
+         // toTupleSignal6 ,
+         // toTupleSignal7 ,
+         // toTupleSignal8 ,
+         // toTupleSignal9 ,
+         toWeakSignal ,
+
+         //
+      }
 
       def scanLeft[State](seed: => State )(doDigest: (State, A) => State )
       : AsyncStateChangeMonad[State]
@@ -77,7 +179,7 @@ object AsyncStateChangeMonad
 opaque type EventMonad[+A]
    <: (
       AnyRef
-      & com.raquo.airstream.core.EventStream[A]
+      // & com.raquo.airstream.core.EventStream[A]
    )
    =  com.raquo.airstream.core.EventStream[A]
 
@@ -90,12 +192,74 @@ object EventMonad
    = identity[com.raquo.airstream.core.EventStream[A] ] _
 
    extension [A] (this10: EventMonad[A] ) {
+      private
       def this11
       /* widen the type, to build on the opaque alias */
-      : com.raquo.airstream.core.BaseObservable[EventMonad, A]
+      : EventMonad[A] & com.raquo.airstream.core.BaseObservable[EventMonad, A]
       = this10
 
-      export this11.{*, given }
+      export this11.{given }
+
+      export this11.{
+      //   changes ,
+        compose ,
+      //   composeAll ,
+      //   composeChanges ,
+      //   currentValueFromParent ,
+        debugWith ,
+      //   defaultDisplayName ,
+        displayName ,
+        setDisplayName ,
+        distinct ,
+        distinctBy ,
+        distinctByFn ,
+        distinctByRef ,
+        distinctErrors ,
+        distinctTry ,
+      //   removeExternalObserver ,
+      //   addExternalObserver ,
+      //   removeExternalObserverNow ,
+        flatMap ,
+        foldLeft ,
+        foldLeftRecover ,
+        foreach ,
+      //   addInternalObserver ,
+      //   removeInternalObserver ,
+      //   removeInternalObserverNow ,
+      //   isSafeToRemoveObserver ,
+      //   isStarted ,
+      //   lastUpdateId ,
+        map ,
+        mapTo ,
+        mapToStrict ,
+        mapToUnit ,
+        matchStreamOrSignal ,
+      //   maybeDisplayName ,
+      //   maybePendingObserverRemovals ,
+      //   maybeWillStart ,
+      //   now ,
+      //   numAllObservers ,
+      //   observe ,
+        addObserver ,
+      //   onAddedExternalObserver ,
+      //   onStart ,
+      //   onStop ,
+      //   onWillStart ,
+      //   getOrCreatePendingObserverRemovals ,
+      //   protectedAccessEvidence ,
+        recover ,
+        recoverIgnoreErrors ,
+        recoverToTry ,
+        scanLeft ,
+        scanLeftRecover ,
+        throwFailure ,
+      //   topoRank ,
+      //   tryNow ,
+        toObservable ,
+        toSignalIfStream ,
+        toStreamIfSignal ,
+        toWeakSignal ,
+      }
 
    }
 
