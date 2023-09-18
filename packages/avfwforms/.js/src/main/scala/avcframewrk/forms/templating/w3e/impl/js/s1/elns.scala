109d932de9726a86e344df7f02536a327930c9e6
me <meh@meh>
2023-09-18 19:55:54 +0700

    [`avcframewrk.forms`] [Templating] [JS] [`impl.js.s1`] apply these (re)organisation (around the def/type-narrowing of `Article` and `LaminarSpawnable`)
    the names of the defs were rather awfully nonsensical ;
    those names likely came out of hurry ; I'm trying to fix it by moving them each into its own class/trait
    
    also
    notice
    a temporary repetition (of the super-class(es) in the self-type(s), into the `extends`es ) there,
    necessary to prevent the compiler from "crashing" or serious 100%-CPU hanging

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/elns.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/elns.scala
@@ -34,6 +34,7 @@ extends
    with ELaminarQckCoreABackreferencings
    with w3e.pre.ArticlesViaLspw
    with ELaminarQckCore1
+   with ELSRNA
    // with w3e.pre.ArticlesViaLspwmt
    /* with these items item merely listed in the self-type, the IDE/editor won't show any relevant "overrides super member" markers */
    with w3e.pre.Articles
@@ -83,15 +84,67 @@ extends
       }
    }
 
-   protected[avcframewrk]
-   opaque type SRNA
-      [
-         -mdlT,
-         +SpawnedLaminar <: ln.ReactiveElement[?],
-         +ReconcOpR,
-      ]
-   <: LReconciliativeKeyTo1[LReconciliativeKey.ScnAllocAndReconcileAndDistillH[?, mdlT, SpawnedLaminar ] ]
-   =  LReconciliativeKeyTo1[LReconciliativeKey.ScnAllocAndReconcileAndDistillH[?, mdlT, SpawnedLaminar ] ]
+   ;
+}
+
+/** 
+ * 
+ * narrows `Article` to the necessary sub-type.
+ * 
+ */
+trait ELaminarQckCoreHtml
+extends
+   AnyRef
+   /* with these items item merely listed in the self-type, the IDE/editor won't show any relevant "overrides super member" markers */
+   with ELaminarQckCore
+{
+   this : (
+      AnyRef
+      with ELaminarQckCore
+   ) =>
+   ;
+
+   import com.raquo.laminar.{nodes as ln}
+
+   import org.scalajs.dom
+
+   override
+   type Article
+   >: LaminarSpawnable[ln.ReactiveHtmlElement[? <: dom.HTMLElement] , dom.HTMLElement ] | LaminarSpawnable[ln.ReactiveSvgElement[? <: dom.SVGElement] , dom.SVGElement ]
+   <: LaminarSpawnable[ln.ReactiveHtmlElement[? <: dom.HTMLElement] , dom.HTMLElement ] | LaminarSpawnable[ln.ReactiveSvgElement[? <: dom.SVGElement] , dom.SVGElement ]
+
+   ;
+}
+
+trait ELSRNA
+extends
+   AnyRef
+   // with w3e.pre.ArticlesViaLspwmt
+   /* with these items item merely listed in the self-type, the IDE/editor won't show any relevant "overrides super member" markers */
+   // that's all
+   /* a temporary repetition here (of below) necessary to prevent the compiler from "crashing" or serious 100%-CPU hanging */
+   // with w3e.pre.ArticlesViaLspw
+   // with w3e.pre.Articles
+   // with w3e.pre.ArticlesViaLspw
+   with w3e.pre.ArticlesImplByL
+   with ELaminarQckCoreLrks
+{
+   this : (
+      AnyRef
+      with w3e.pre.Articles
+      with w3e.pre.ArticlesViaLspw
+      with w3e.pre.ArticlesImplByL
+      // with ELaminarQckCore1
+      with ELaminarQckCoreLrks
+      // with ELaminarQckCoreABackreferencings
+   ) =>
+   ;
+
+   import com.raquo.laminar.{nodes as ln}
+
+   import org.scalajs.dom
+
+   ;
 
    /** 
     * warning - this's add a reg into the registry (with)in the NS `LReconciliativeKey`, leading to "memory leak"!
@@ -137,36 +190,20 @@ extends
       ;
    }
 
-   ;
-}
-
-/** 
- * 
- * narrows `Article` to the necessary sub-type.
- * 
- */
-trait ELaminarQckCoreHtml
-extends
-   AnyRef
-   /* with these items item merely listed in the self-type, the IDE/editor won't show any relevant "overrides super member" markers */
-   with ELaminarQckCore
-{
-   this : (
-      AnyRef
-      with ELaminarQckCore
-   ) =>
-   ;
-
-   import com.raquo.laminar.{nodes as ln}
-
-   import org.scalajs.dom
-
-   override
-   type Article
-   >: LaminarSpawnable[ln.ReactiveHtmlElement[? <: dom.HTMLElement] , dom.HTMLElement ] | LaminarSpawnable[ln.ReactiveSvgElement[? <: dom.SVGElement] , dom.SVGElement ]
-   <: LaminarSpawnable[ln.ReactiveHtmlElement[? <: dom.HTMLElement] , dom.HTMLElement ] | LaminarSpawnable[ln.ReactiveSvgElement[? <: dom.SVGElement] , dom.SVGElement ]
+   protected[avcframewrk]
+   opaque type SRNA
+      [
+         -mdlT,
+         +SpawnedLaminar <: ln.ReactiveElement[?],
+         +ReconcOpR,
+      ]
+   <: LReconciliativeKeyTo1[LReconciliativeKey.ScnAllocAndReconcileAndDistillH[?, mdlT, SpawnedLaminar ] ]
+   =  LReconciliativeKeyTo1[LReconciliativeKey.ScnAllocAndReconcileAndDistillH[?, mdlT, SpawnedLaminar ] ]
 
    ;
+
+   ;
+
 }
 
 
