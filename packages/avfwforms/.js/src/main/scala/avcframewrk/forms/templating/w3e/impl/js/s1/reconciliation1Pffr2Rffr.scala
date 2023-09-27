d77077fa6ae2ff2f62924356cd1bcd4e772b0cc9
me <meh@meh>
2023-09-27 15:59:18 +0700

    [`avcframewrk.forms`] [Templating] [JS] [`impl.js.s1`] apply these changes

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/reconciliation1Pffr2Rffr.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/w3e/impl/js/s1/reconciliation1Pffr2Rffr.scala
@@ -65,16 +65,75 @@ extends
       ReconcOpR
       >: Unit <: Unit
       ,
-   ] (perFrameReconciler00: SpawnabilityAndReconciliabilityNoArg[Option[ImplSpecificPerFrameMdl], S, ReconcOpR] )
+
+      //
+      SpecificSpawnability
+      <: SpawnabilityAndReconciliabilityNoArg[?, S, ReconcOpR]
+      ,
+   ]
+      //
+      (perFrameReconciler00: SpecificSpawnability )
+      (using unpk : Pffr2RffrGivenUnpck1[ImplSpecificPerFrameMdl ] )
+      (using specificSpawnabilityConforming : SpecificSpawnability <:< SpawnabilityAndReconciliabilityNoArg[unpk.XPackedly[ImplSpecificPerFrameMdl], S, ReconcOpR] )
    {
       def asPffr2RffrGiven
-      = Pffr2RffrGiven(perFrameReconciler00)
+         //
+      = Pffr2RffrGiven(specificSpawnabilityConforming(perFrameReconciler00) )
 
       export asPffr2RffrGiven.{* }
    }
 
+   object extraPffr2RffrGivenUnpck
+   {
+      ;
+
+      protected
+      final
+      lazy
+      val defaultPffr2RffrGivenUnpckImpl
+      = Pffr2RffrGivenUnpckBy[[E] =>> Option[E] ] (
+         //
+         [E] => (e: E) => Some(e)
+         ,
+         None ,
+      )
+
+      given defaultPffr2RffrGivenUnpck
+      : defaultPffr2RffrGivenUnpckImpl.type
+      = defaultPffr2RffrGivenUnpckImpl
+
+      ;
+   }
+
+   /** 
+    * by default it should wrap in `Option`, but
+    * sometimes it's inappropriate ;
+    * `given` a custom instance of this,
+    * to customise it
+    * 
+    */
+   type Pffr2RffrGivenUnpck
+   = Pffr2RffrGivenUnpck1[Any]
+
    private[s1]
-   case class Pffr2RffrGiven [
+   trait Pffr2RffrGivenUnpck1[-B]
+   {
+      ;
+
+      type XPackedly[+_ <: B ]
+
+      def pack[E <: B](e: E)
+      : XPackedly[e.type]
+
+      def fromNone
+      : XPackedly[Nothing]
+
+      ;
+   }
+
+   private[s1]
+   implicit
+   class Pffr2RffrGiven [
       ImplSpecificPerFrameMdl
       ,
       S
@@ -83,7 +142,20 @@ extends
       ReconcOpR
       >: Unit <: Unit
       ,
-   ] (perFrameReconciler00: SpawnabilityAndReconciliabilityNoArg[Option[ImplSpecificPerFrameMdl], S, ReconcOpR] ) {
+
+      +SpecificUnpk <: (
+         Pffr2RffrGivenUnpck1[ImplSpecificPerFrameMdl ]
+         & Singleton
+      ) 
+      ,
+
+      //
+   ] 
+      //
+      //
+      (using val unpk : SpecificUnpk)
+      (perFrameReconciler00: SpawnabilityAndReconciliabilityNoArg[unpk.XPackedly[ImplSpecificPerFrameMdl], S, ReconcOpR] )
+   {
       //
 
       /** 
@@ -108,6 +180,11 @@ extends
                rc.spawnAsXsr(mdl )
                .mapHl(_.wrappedLaminarElemI  )
             } )
+
+            .compose((e: Option[UserSideMdl] ) => {
+               System.out.nn.write(IArray.emptyByteIArray.toArray )
+               e
+            })
          } }
 
          match { case e => e }
@@ -120,7 +197,7 @@ extends
          contraFlatMap1OImpl(implImplSideAnimFramesFromUserSideMdl )
       }
 
-      private[Pffr2RffrGiven ] 
+      private[s1]
       class contraFlatMap1OImpl
          [UserSideMdl ]
          (implImplSideAnimFramesFromUserSideMdl: UserSideMdl => L.Signal[ImplSpecificPerFrameMdl] )
@@ -161,6 +238,8 @@ extends
 
          }
 
+         System.currentTimeMillis()
+
          val repipingImplReconciler
             /* can't make the type inferred, due to the resulting Cyclic Error .. */
          : (
@@ -188,7 +267,7 @@ extends
                =  L.Var[Int]
 
                val e1
-               = perFrameReconciler.spawn(None )( )
+               = perFrameReconciler.spawn(unpk.fromNone )( )
 
                val internalReconcileTo
                = {
@@ -196,6 +275,7 @@ extends
                   match { case f : Function1[aType, r] => {
                      f.compose((a: aType ) => { frameCounter.update(_ + 1 ) ; a } )
                   } }
+                  match { case f => f.compose[Option[ImplSpecificPerFrameMdl] ](o => o.fold(unpk.fromNone)(e => unpk.pack(e) ) ) }
                }
 
                ;
@@ -229,6 +309,12 @@ extends
 
                repiping1.signal
 
+               .composeChanges(s => {
+                  // TODO
+                  // s.delay(15 )
+                  s.delay(815 )
+               } )
+
                .flatMap({
 
                   case None =>
@@ -244,15 +330,56 @@ extends
                      val eFrames
                      = implImplSideAnimFramesFromUserSideMdl(desc)
 
+                     ({
+                     ;
+
                      for {
                         eFrame <- eFrames
                      }
                      yield (() => {
                         internalReconcileTo(Some(eFrame ) )
                      } )
+                     }) 
+
+                     // .composeChanges(s => {
+                     //    // TODO
+                     //    // s.delay(15 )
+                     //    s
+                     //    .delay(815 )
+                     // } )
+                     .map(<:<.refl )
+
 
                })
-               .foreach(_.apply() )
+               .map(_.apply() )
+               
+               // TODO
+               match {
+                  case chn => {
+                     ;
+
+                     implicit
+                     val o = com.raquo.airstream.ownership.ManualOwner()
+
+                     chn
+                     .recover({
+                        case util.control.NonFatal(z) =>
+                           ;
+                           import org.scalajs.dom.console
+                           // throw
+                           //    new LlrConHException(s"exception: ${z} ", z)
+                           console.error(s"exception:")
+                           console.infoExceptionCollapsed({
+                              z
+                              // new LlrConHException(s"${z } " , z )
+                           }, headline = z.getClass().getName().nn )
+                           console.warn(s"removing the subscription")
+                           o.killSubscriptions()
+                           Some(() )
+                     })
+                     .observe
+                  }
+               }
 
                ;
             }
@@ -325,6 +452,130 @@ extends
 
       //
    }
+
+   /** 
+    * a factory method for `Pffr2RffrGivenUnpck1` .
+    * avoid using this variant, use the other one instead.
+    * 
+    */
+   @deprecated
+   def Pffr2RffrGivenUnpckBy
+      [C[+_] ]
+      (c: [E] => E => C[E], cx: => C[Nothing] )
+   = {
+      Pffr2RffrGivenUnpckBy1[C, Any ](
+         //
+         (x: Any) => c(x)
+         ,
+         cx
+         ,
+      )
+   }
+
+   /** 
+    * a factory method for `Pffr2RffrGivenUnpck1`
+    * 
+    */
+   def Pffr2RffrGivenUnpckBy1
+      [C[+_ <: B], B ]
+      (c: (x: B) => C[x.type] , cx: => C[Nothing] )
+   = {
+      new
+         AnyRef
+         with Pffr2RffrGivenUnpck1[B]
+         with Selectable
+      {
+         ;
+         type XPackedly[+E <: B ]
+         >: C[E]
+         <: C[E]
+         ;
+         //
+         def pack[E <: B ](e: E)
+         = c(e )
+         ;
+         def fromNone
+         = cx
+      }
+   }
+
+   def Pffr2RffrGivenUnpckByOption1
+      [CSme[+_ <: B ], B ] 
+      (c: (x: B) => CSme[x.type ] , cx: => B )
+   = Pffr2RffrGivenUnpckByOption[CSme, B, B ](c, cx )
+
+   def Pffr2RffrGivenUnpckByOption
+      [CSme[+_ <: B ], B, CNe ] 
+      (c: (x: B) => CSme[x.type ] , cx: => CNe )
+   = {
+      Pffr2RffrGivenUnpckBy1[[SpecificB <: B] =>> (CSme[SpecificB] | CNe ) , B ](
+         //
+         (x: B ) => c(x)
+         ,
+         cx
+         ,
+      )
+   }
+
+   extension [EB0] (impl: Pffr2RffrGivenUnpck1[? >: EB0] ) {
+      //
+
+      def contramap
+         //
+         [C1[+_ <: EB ] <: EB0, EB ]
+         (f: (e: EB) => C1[e.type] )
+         (using reflect.Typeable[EB] )
+      = {
+
+         Pffr2RffrGivenUnpckBy1
+            [[E <: EB ] =>> impl.XPackedly[C1[E] ], EB ]
+            (
+               //
+               (x0: EB) => {
+                  x0
+                  match { case x : (x0.type & EB) => f(x) }
+                  match { case r => impl.pack(r) }
+                  match { case r => (r) }
+               }
+               ,
+               (impl.fromNone )
+               ,
+            )
+      }
+
+      //
+   }
+
+   extension (impl: Pffr2RffrGivenUnpck1[Any] )
+      @deprecated
+      def map
+         [C1[+_] ]
+         (f0: (impl1 : impl.type) ?=> [E] => impl1.XPackedly[E] => C1[E] )
+      = {
+         ;
+
+         given impl.type
+         = impl
+
+         val f
+         : ([E] => (x: impl.XPackedly[E]) => C1[E])
+         = f0(using impl)
+
+         Pffr2RffrGivenUnpckBy
+            [C1]
+            (
+               //
+               [E] => (x: E) => {
+                  impl.pack(x)
+                  match { case r => f(r) }
+                  match { case r => (r) }
+               }
+               ,
+               f(impl.fromNone )
+               ,
+            )
+      }
+
 
 
    ;
