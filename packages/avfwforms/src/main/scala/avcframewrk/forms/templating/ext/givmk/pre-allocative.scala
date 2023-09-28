df7b360ec324a64fd3faa48752fdaf78966faed9
me <meh@meh>
2023-09-28 12:53:35 +0700

    [`avcframewrk.forms`] [Templating] [GIVMK] [the backend-agnostic tree] apply these enhncs

---
+++ packages/avfwforms/src/main/scala/avcframewrk/forms/templating/ext/givmk/pre-allocative.scala
@@ -0,0 +1,47 @@
+package avcframewrk.forms
+
+package templating
+
+package ext.givmk
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+protected
+trait TypeBoxLikeAllocatorImpl[-L, +U ]
+{
+   type CAP
+   >: L
+   <: U
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
