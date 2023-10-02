57aed9704ff71f46ae937487ea0662debb7f04b6
me <meh@meh>
2023-10-02 19:29:40 +0700

    [`avcframewrk.forms`] [Templating] [GIVMK] [JS] apply these changes

---
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/ext/givmk/impl/eventsImpl.scala
@@ -0,0 +1,53 @@
+package avcframewrk.forms
+
+package templating
+
+package ext.givmk
+
+package impl
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import org.scalajs.dom
+
+import com.raquo.laminar.{nodes as ln }
+
+final
+lazy val llBasedEventCtxAllocator
+=  Event._Allocator.forType[dom.Event ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+val _ = {}
