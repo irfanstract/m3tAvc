b5e03b69fd0eea07356d4796a65a0001ed289fa4
me <meh@meh>
2023-10-02 19:29:40 +0700

    [`avcframewrk.forms`] [Templating] [GIVMK] [JS] apply these changes

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/ext/givmk/impl/lspw.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/ext/givmk/impl/lspw.scala
@@ -45,14 +45,21 @@ object Lspw
    ;
 
    type _Base
-   = Impl[? ]
+   = Impl[Nothing, Any ]
+
+   opaque type Impl
+      [
+         -MdlL <: MdlU, +MdlU ,
+      ]
+   <: AnyRef
+   =  ImplReconcilerAndMdlPair[? >: MdlL <: MdlU ]
  
    extension (receiver: Lspw._Base ) {
       //
 
       private
       def receiverLspwBaseForSpw
-      : receiver.type
+      : receiver.type & ImplReconcilerAndMdlPair[? ]
       = valueOf
 
       export receiverLspwBaseForSpw.spawn
@@ -61,63 +68,63 @@ object Lspw
 
    }
 
-   private[givmk]
-   case class Impl
-      [Mdl]
-      (protected val impl: (
-         ScnAllocAndReconcileAndDistillH[?, Mdl, AcceptableLE ] ,
-         Mdl ,
-      ) )
-   {
+   ;
+}
+
+private[givmk]
+case class ImplReconcilerAndMdlPair
+   [Mdl]
+   (protected val impl: (
+      ScnAllocAndReconcileAndDistillH[?, Mdl, AcceptableLE ] ,
+      Mdl ,
+   ) )
+{
+   ;
+
+   export impl.{_2 as mdl }
+
+   export impl.{_1 as sbReconciler }
+
+   // export sbReconciler.{IntermediateState as CompIntermediateState }
+   opaque type CompIntermediateState
+   >: sbReconciler.IntermediateState
+   <: sbReconciler.IntermediateState
+   =  sbReconciler.IntermediateState
+
+   /* work-around to the eager widening of wildcard-types */
+   val sbReconciler1
+   : ScnAllocAndReconcileAndDistillH[CompIntermediateState, Mdl, AcceptableLE ]
+   = {
       ;
-      
-      export impl.{_2 as mdl }
-
-      export impl.{_1 as sbReconciler }
-
-      // export sbReconciler.{IntermediateState as CompIntermediateState }
-      opaque type CompIntermediateState
-      >: sbReconciler.IntermediateState
-      <: sbReconciler.IntermediateState
-      =  sbReconciler.IntermediateState
-
-      /* work-around to the eager widening of wildcard-types */
-      val sbReconciler1
-      : ScnAllocAndReconcileAndDistillH[CompIntermediateState, Mdl, AcceptableLE ]
-      = {
-         ;
-         sbReconciler
-         .pipeExactSelf({
-            implicitly[<:<[sbReconciler.type, ScnAllocAndReconcileAndDistillH[CompIntermediateState, Mdl, AcceptableLE ] ] ]
-         })
-      }
-
-      export sbReconciler.{given_SpawnabilityAndReconciliabilityNoArg_Md_TIntermediateState as pairwiseReconciler }
-
-      def spawn
-         //
-         ()
-      : CompIntermediateState
-      = {
-         ;
-         ;
-         sbReconciler1.spawnMd(mdl )
-         .tapLooseSelf(e => {} )
-      }
-
-      def reconcile
-         //
-         (sExisting: CompIntermediateState )
-      = {
-         ;
-         ;
-         sbReconciler1.reconcile(sExisting , mdl )
-         .tapLooseSelf(e => {} )
-      }
-
+      sbReconciler
+      .pipeExactSelf({
+         implicitly[<:<[sbReconciler.type, ScnAllocAndReconcileAndDistillH[CompIntermediateState, Mdl, AcceptableLE ] ] ]
+      })
    }
 
-   ;
+   export sbReconciler.{given_SpawnabilityAndReconciliabilityNoArg_Md_TIntermediateState as pairwiseReconciler }
+
+   def spawn
+      //
+      ()
+   : CompIntermediateState
+   = {
+      ;
+      ;
+      sbReconciler1.spawnMd(mdl )
+      .tapLooseSelf(e => {} )
+   }
+
+   def reconcile
+      //
+      (sExisting: CompIntermediateState )
+   = {
+      ;
+      ;
+      sbReconciler1.reconcile(sExisting , mdl )
+      .tapLooseSelf(e => {} )
+   }
+
 }
 
 object rcKeyingDefiner1
