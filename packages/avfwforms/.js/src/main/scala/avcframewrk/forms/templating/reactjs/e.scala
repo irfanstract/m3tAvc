6a0397f7fa3eda8ab8364bd0a234b53226dbb8e7
me <meh@meh>
2023-10-09 08:03:44 +0700

    [`avcframewrk.forms`] [the JS-specific `templating.reactjs`] [`KS.*` in `e.scala`] impl `slToExprValueOfGvns`

--- packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/reactjs/e.scala
+++ packages/avfwforms/.js/src/main/scala/avcframewrk/forms/templating/reactjs/e.scala
@@ -36,6 +36,68 @@ object KS
 
    ;
 
+   object slToExprValueOfGvns {
+      ;
+
+      import compiletime.*
+      import quoted.*
+
+      @deprecated
+      given picklingInstance[T : ValueOf : Type ]
+      : ToExpr[T]
+      with {
+         def apply(t: T)(using Quotes )
+         = {
+            import quotes.reflect.*
+            '{
+               given tSinglecy
+               : ValueOf[T]
+               = ${
+                  Expr.summon[ValueOf[T] ]
+                  .getOrElse(report.errorAndAbort("cannot re-resolve the ValueOf[T]") )
+               }
+               valueOf[T]
+            }
+         }
+      }
+
+      @deprecated
+      // transparent
+      inline
+      given unpicklingInstance[T ]
+      : FromExpr[T]
+      = {
+         ;
+         val r
+         = summonFrom({ case e : ValueOf[T] => Some(e) ; case _ => None })
+         new FromExpr[T]
+         {
+            def unapply(t: Expr[T] )(using quoted.Quotes )
+            = for ( r1 <- r ) yield {
+               given r1.type = r1
+               valueOf[T]
+            }
+         }
+      }
+
+      object tvoImplicits {
+         ;
+
+         ;
+
+         given vwOfT[T : Type ] (using Quotes )
+         : ValueOf[T]
+         = {
+            import quotes.reflect.*
+            ( for (value <- Type.valueOfConstant[T] ) yield ValueOf(value ) )
+            .getOrElse(report.errorAndAbort(s"no Type.valueOfConstant[T]. \neligible types include (... ...) ") )
+         }
+
+         ;
+      }
+
+   }
+
    implicit class Sl(receiver: KS )
    extends
    AnyRef
